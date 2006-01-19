548528afa19c3b739dc20d15f46bfafc8a818621
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-01-19 16:53:27 +0000

    Docstring.

--- shotserver/lib/__init__.py
+++ shotserver/lib/__init__.py
@@ -16,6 +16,11 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 # MA 02111-1307, USA.
 
+"""
+Top-level shotserver module.
+All HTTP requests are handled from here.
+"""
+
 __revision__ = '$Rev$'
 __date__     = '$Date$'
 __author__   = '$Author$'
