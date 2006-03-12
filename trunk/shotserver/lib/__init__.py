815754a681ddf51cf2289143f4314c16a4727413
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-03-12 23:10:02 +0000

    Simple test suite, just type 'make test' before committing.

--- trunk/shotserver/lib/__init__.py
+++ trunk/shotserver/lib/__init__.py
@@ -1,5 +1,5 @@
-# Browsershots
-# Copyright (C) 2006 Johann C. Rocholl <johann@rocholl.net>
+# browsershots.org
+# Copyright (C) 2006 Johann C. Rocholl <johann@browsershots.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -24,3 +24,42 @@ All HTTP requests are handled from here.
 __revision__ = '$Rev$'
 __date__     = '$Date$'
 __author__   = '$Author$'
+
+from mod_python import apache
+from interface import xhtml
+
+def write_html_head():
+    req.write('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"')
+    req.write(' "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">\n')
+    xhtml.write_open_tag_line('html', xmlns="http://www.w3.org/1999/xhtml")
+
+def handler(req):
+    """
+    Process all incoming HTTP requests.
+    """
+    try:
+        __builtins__['req'] = req
+        req.info = request.RequestInfo()
+
+        req.status = apache.OK
+        req.content_type = 'application/xhtml+xml; charset=UTF-8'
+
+    except:
+        if naked == 'redirect':
+            write_html_head()
+
+        while len(xhtml.open_tags) > 2:
+            xhtml.write_close_tag_line()
+        if xhtml.open_tags[-1] == 'head':
+            xhtml.write_close_tag_line('head')
+        if xhtml.open_tags[-1] != 'body':
+            xhtml.write_open_tag_line('body')
+        # xhtml.write_tag_line('br')
+        xhtml.write_tag_line('p', 'Internal error:', _class="error")
+        trace = ''.join(traceback.format_exception(*sys.exc_info()))
+        trace = trace.replace('<', '&lt;')
+        trace = trace.replace('>', '&gt;')
+        xhtml.write_tag_line('pre', trace)
+        xhtml.write_close_tag_line('body')
+        xhtml.write_close_tag_line('html')
+        return apache.OK
--- trunk/shotserver/scripts/shotserver03_db_create.py
+++ trunk/shotserver/scripts/shotserver03_db_create.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
+#! /usr/bin/python
 
-# Browsershots
+# browsershots.org
 # Copyright (C) 2006 Johann C. Rocholl <johann@browsershots.org>
 #
 # This program is free software; you can redistribute it and/or modify
