826661e24c0ffd582b0f1f497cd2ca1b97bb5f70
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-03-29 06:16:04 +0000

    More browsers.

--- shotserver/sql/test_insert.sql
+++ shotserver/sql/test_insert.sql
@@ -8,27 +8,45 @@ INSERT INTO browser (browser_name) VALUES ('Galeon');
 INSERT INTO browser (browser_name) VALUES ('Mozilla');
 INSERT INTO browser (browser_name) VALUES ('Epiphany');
 INSERT INTO browser (browser_name) VALUES ('Opera');
+INSERT INTO browser (browser_name) VALUES ('Camino');
+INSERT INTO browser (browser_name) VALUES ('Links');
+INSERT INTO browser (browser_name) VALUES ('Lynx');
+INSERT INTO browser (browser_name) VALUES ('W3M');
+INSERT INTO browser (browser_name) VALUES ('NetFront');
+INSERT INTO browser (browser_name) VALUES ('Blazer');
+INSERT INTO browser (browser_name) VALUES ('EudoraWeb');
 
-INSERT INTO browser_version (browser, browser_major, browser_minor, browser_engine) VALUES (1, 1, 5, 'Gecko');
-INSERT INTO browser_version (browser, browser_major, browser_minor, browser_engine) VALUES (2, 2, 0, 'KHTML');
-INSERT INTO browser_version (browser, browser_major, browser_minor, browser_engine) VALUES (1, 1, 0, 'Gecko');
-INSERT INTO browser_version (browser, browser_major, browser_minor, browser_engine) VALUES (4, 3, 2, 'KHTML');
-INSERT INTO browser_version (browser, browser_major, browser_minor, browser_engine) VALUES (8, 8, 5, 'Opera');
+INSERT INTO engine (engine_name) VALUES ('Gecko');
+INSERT INTO engine (engine_name) VALUES ('KHTML');
+
+INSERT INTO browser_version (browser, browser_major, browser_minor, engine) VALUES (1, 1, 5, 1);
+INSERT INTO browser_version (browser, browser_major, browser_minor, engine) VALUES (2, 2, 0, 2);
+INSERT INTO browser_version (browser, browser_major, browser_minor, engine) VALUES (1, 1, 0, 1);
+INSERT INTO browser_version (browser, browser_major, browser_minor, engine) VALUES (4, 3, 2, 2);
+INSERT INTO browser_version (browser, browser_major, browser_minor) VALUES (8, 8, 5);
+INSERT INTO browser_version (browser, browser_major, browser_minor, engine) VALUES (1, 1, 0, 1);
+INSERT INTO browser_version (browser, browser_major, browser_minor, engine) VALUES (9, 1, 0, 1);
+INSERT INTO browser_version (browser, browser_major, browser_minor) VALUES (11, 2, 8);
+INSERT INTO browser_version (browser, browser_major, browser_minor) VALUES (12, 0, 5);
+INSERT INTO browser_version (browser, browser_major, browser_minor) VALUES (13, 1, 0);
 
 INSERT INTO os (os_name) VALUES ('Linux');
 INSERT INTO os (os_name, os_manufacturer) VALUES ('Mac OS', 'Apple');
 INSERT INTO os (os_name, os_manufacturer) VALUES ('Windows', 'Microsoft');
+INSERT INTO os (os_name, os_manufacturer) VALUES ('Palm OS', 'Palm');
 
 INSERT INTO os_version (os, os_distro, os_codename, os_major, os_minor) VALUES (1, 'Ubuntu', 'Dapper Drake', 6, 4);
 INSERT INTO os_version (os, os_distro, os_codename, os_major, os_minor) VALUES (2, 'X', 'Tiger', 10, 4);
 INSERT INTO os_version (os, os_distro, os_codename) VALUES (3, 'XP', 'Service Pack 2');
 INSERT INTO os_version (os, os_distro, os_codename, os_major, os_minor) VALUES (1, 'Debian', 'Sarge', 3, 1);
+INSERT INTO os_version (os, os_codename, os_major, os_minor) VALUES (4, 'Cobalt', 6, 1);
 
 INSERT INTO factory (factory_name, os_version, factory_admin) VALUES ('tyll', 1, 1);
 INSERT INTO factory (factory_name, os_version, factory_admin) VALUES ('runt', 2, 1);
 INSERT INTO factory (factory_name, os_version, factory_admin) VALUES ('sven', 3, 1);
 INSERT INTO factory (factory_name, os_version, factory_admin) VALUES ('quad', 3, 1);
 INSERT INTO factory (factory_name, os_version, factory_admin) VALUES ('azul', 4, 1);
+INSERT INTO factory (factory_name, os_version, factory_admin) VALUES ('palm', 5, 1);
 
 INSERT INTO factory_browser (factory, browser_version) VALUES (1, 1);
 INSERT INTO factory_browser (factory, browser_version) VALUES (2, 2);
