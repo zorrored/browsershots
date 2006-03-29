957e1ccca1b82fef7d04df46cdd0ce4220be257d
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-03-29 07:28:22 +0000

    Fixed natural joins.

--- trunk/shotserver/sql/test_select.sql
+++ trunk/shotserver/sql/test_select.sql
@@ -1,13 +1,15 @@
 SELECT * FROM person;
-SELECT * FROM os;
+SELECT * FROM os NATURAL JOIN os_version;
+SELECT * FROM browser NATURAL JOIN browser_version;
 
 SELECT factory_name, os_name, os_manufacturer,
 os_distro, os_codename, os_major, os_minor, person_name AS admin_name
 FROM factory NATURAL JOIN os_version NATURAL JOIN os NATURAL JOIN person
 ORDER BY factory_name;
 
-SELECT factory_name, browser_manufacturer, browser_name, browser_major, browser_minor, terminal, mobile, engine_name
-FROM factory_browser NATURAL JOIN factory NATURAL JOIN browser_version NATURAL JOIN browser NATURAL JOIN engine
+SELECT factory_name, browser_manufacturer, browser_name, browser_major, browser_minor, terminal, mobile
+FROM factory_browser NATURAL JOIN factory NATURAL JOIN os_version NATURAL JOIN os
+NATURAL JOIN browser_version NATURAL JOIN browser
 ORDER BY factory_name;
 
 SELECT * FROM request;
