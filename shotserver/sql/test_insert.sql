bdea44eb09a56eee5f24cfff029a330ef87cfa80
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-04-02 20:11:00 +0000

    More database testing.

--- shotserver/sql/test_insert.sql
+++ shotserver/sql/test_insert.sql
@@ -1,5 +1,14 @@
 INSERT INTO person (name, email) VALUES ('Johann C. Rocholl', 'jcrocholl@browsershots.org');
 
+INSERT INTO platform (name, creator) VALUES ('i386', 1);
+INSERT INTO platform (name, creator) VALUES ('PPC', 1);
+INSERT INTO platform (name, creator) VALUES ('68000', 1);
+INSERT INTO platform (name, creator) VALUES ('Palm', 1);
+INSERT INTO platform (name, creator) VALUES ('AMD64', 1);
+
+INSERT INTO engine (name, creator) VALUES ('Gecko', 1);
+INSERT INTO engine (name, creator) VALUES ('KHTML', 1);
+
 INSERT INTO browser (name, manufacturer, creator) VALUES ('Firefox', 'Mozilla', 1);
 INSERT INTO browser (name, manufacturer, creator) VALUES ('Safari', 'Apple', 1);
 INSERT INTO browser (name, manufacturer, creator) VALUES ('MSIE', 'Microsoft', 1);
@@ -16,46 +25,63 @@ INSERT INTO browser (name, creator) VALUES ('NetFront', 1);
 INSERT INTO browser (name, creator) VALUES ('Blazer', 1);
 INSERT INTO browser (name, creator) VALUES ('EudoraWeb', 1);
 
-INSERT INTO engine (name, creator) VALUES ('Gecko', 1);
-INSERT INTO engine (name, creator) VALUES ('KHTML', 1);
-
 INSERT INTO browser_version (browser, major, minor, engine, creator) VALUES (1, 1, 5, 1, 1);
 INSERT INTO browser_version (browser, major, minor, engine, creator) VALUES (2, 2, 0, 2, 1);
-INSERT INTO browser_version (browser, major, minor, engine, creator) VALUES (1, 1, 0, 1, 1);
+INSERT INTO browser_version (browser, major, minor, creator) VALUES (3, 6, 0, 1);
 INSERT INTO browser_version (browser, major, minor, engine, creator) VALUES (4, 3, 3, 2, 1);
+INSERT INTO browser_version (browser, major, minor, engine, creator) VALUES (5, 1, 3, 1, 1);
+INSERT INTO browser_version (browser, major, minor, engine, creator) VALUES (6, 1, 7, 1, 1);
+INSERT INTO browser_version (browser, major, minor, engine, creator) VALUES (7, 1, 4, 1, 1);
 INSERT INTO browser_version (browser, major, minor, creator) VALUES (8, 8, 5, 1);
 INSERT INTO browser_version (browser, major, minor, engine, creator) VALUES (9, 1, 0, 1, 1);
+INSERT INTO browser_version (browser, major, minor, creator) VALUES (10, 1, 0, 1);
 INSERT INTO browser_version (browser, major, minor, creator) VALUES (11, 2, 8, 1);
 INSERT INTO browser_version (browser, major, minor, creator) VALUES (12, 0, 5, 1);
 INSERT INTO browser_version (browser, major, minor, creator) VALUES (13, 1, 0, 1);
-INSERT INTO browser_version (browser, major, minor, creator) VALUES (3, 6, 0, 1);
+INSERT INTO browser_version (browser, major, minor, creator) VALUES (14, 1, 0, 1);
+INSERT INTO browser_version (browser, major, minor, creator) VALUES (15, 1, 0, 1);
+INSERT INTO browser_version (browser, major, minor, engine, creator) VALUES (1, 1, 0, 1, 1);
 INSERT INTO browser_version (browser, major, minor, creator) VALUES (3, 7, 0, 1);
 
 INSERT INTO os (name, creator) VALUES ('Linux', 1);
 INSERT INTO os (name, manufacturer, creator) VALUES ('Mac OS', 'Apple', 1);
 INSERT INTO os (name, manufacturer, creator) VALUES ('Windows', 'Microsoft', 1);
-INSERT INTO os (name, mobile, creator) VALUES ('Palm OS', TRUE, 1);
+INSERT INTO os (name, creator) VALUES ('Palm OS', 1);
+INSERT INTO os (name, creator) VALUES ('Symbian', 1);
+INSERT INTO os (name, creator) VALUES ('Solaris', 1);
+INSERT INTO os (name, creator) VALUES ('TOS', 1);
 
 INSERT INTO os_version (os, distro, codename, major, minor, creator) VALUES (1, 'Ubuntu', 'Dapper Drake', 6, 4, 1);
 INSERT INTO os_version (os, distro, codename, major, minor, creator) VALUES (2, 'X', 'Tiger', 10, 4, 1);
 INSERT INTO os_version (os, distro, codename, creator) VALUES (3, 'XP', 'Service Pack 2', 1);
+INSERT INTO os_version (os, codename, major, minor, mobile, creator) VALUES (4, 'Cobalt', 6, 1, TRUE, 1);
+INSERT INTO os_version (os, major, minor, mobile, creator) VALUES (5, 6, 1, TRUE, 1);
+INSERT INTO os_version (os, major, minor, creator) VALUES (6, 5, 8, 1);
+INSERT INTO os_version (os, major, minor, creator) VALUES (7, 2, 6, 1);
 INSERT INTO os_version (os, distro, codename, major, minor, creator) VALUES (1, 'Debian', 'Sarge', 3, 1, 1);
-INSERT INTO os_version (os, codename, major, minor, creator) VALUES (4, 'Cobalt', 6, 1, 1);
 
-INSERT INTO factory (name, os_version, creator) VALUES ('tyll', 1, 1);
-INSERT INTO factory (name, os_version, creator) VALUES ('runt', 2, 1);
-INSERT INTO factory (name, os_version, creator) VALUES ('sven', 3, 1);
-INSERT INTO factory (name, os_version, creator) VALUES ('quad', 3, 1);
-INSERT INTO factory (name, os_version, creator) VALUES ('azul', 4, 1);
-INSERT INTO factory (name, os_version, creator) VALUES ('palm', 5, 1);
+INSERT INTO factory (name, os_version, platform, creator) VALUES ('tyll', 1, 1, 1);
+INSERT INTO factory (name, os_version, platform, creator) VALUES ('runt', 2, 2, 1);
+INSERT INTO factory (name, os_version, platform, creator) VALUES ('quad', 3, 1, 1);
+INSERT INTO factory (name, os_version, platform, creator) VALUES ('palm', 4, 4, 1);
+INSERT INTO factory (name, os_version, platform, creator) VALUES ('7650', 5, 4, 1);
+INSERT INTO factory (name, os_version, platform, creator) VALUES ('lara', 6, 3, 1);
+INSERT INTO factory (name, os_version, platform, creator) VALUES ('tari', 7, 3, 1);
+INSERT INTO factory (name, os_version, platform, creator) VALUES ('azul', 8, 5, 1);
+INSERT INTO factory (name, os_version, platform, creator) VALUES ('sven', 3, 1, 1);
 
 INSERT INTO factory_browser (factory, browser_version) VALUES (1, 1);
 INSERT INTO factory_browser (factory, browser_version) VALUES (2, 2);
-INSERT INTO factory_browser (factory, browser_version) VALUES (5, 3);
-INSERT INTO factory_browser (factory, browser_version) VALUES (5, 4);
-INSERT INTO factory_browser (factory, browser_version) VALUES (5, 5);
-INSERT INTO factory_browser (factory, browser_version) VALUES (3, 11);
-INSERT INTO factory_browser (factory, browser_version) VALUES (4, 10);
+INSERT INTO factory_browser (factory, browser_version) VALUES (2, 9);
+INSERT INTO factory_browser (factory, browser_version) VALUES (3, 3);
+INSERT INTO factory_browser (factory, browser_version) VALUES (3, 1);
+INSERT INTO factory_browser (factory, browser_version) VALUES (8, 4);
+INSERT INTO factory_browser (factory, browser_version) VALUES (8, 5);
+INSERT INTO factory_browser (factory, browser_version) VALUES (8, 6);
+INSERT INTO factory_browser (factory, browser_version) VALUES (8, 7);
+INSERT INTO factory_browser (factory, browser_version) VALUES (8, 8);
+INSERT INTO factory_browser (factory, browser_version) VALUES (8, 16);
+INSERT INTO factory_browser (factory, browser_version) VALUES (9, 17);
 
 INSERT INTO factory_screen (factory, width, height) VALUES (1, 1024, 768);
 INSERT INTO factory_screen (factory, width, height) VALUES (1, 800, 600);
--- shotserver/sql/test_select.sql
+++ shotserver/sql/test_select.sql
@@ -4,23 +4,22 @@ SELECT name, major, minor, manufacturer, terminal
 FROM browser JOIN browser_version USING (browser)
 ORDER BY name;
 
-SELECT DISTINCT name, major, minor, manufacturer, mobile
+SELECT DISTINCT name, distro, codename, major, minor, manufacturer, mobile
 FROM os JOIN os_version USING (os)
 ORDER BY name;
 
-SELECT factory.name,
-os.name, os.manufacturer,
+SELECT factory.name, platform.name, os.name, 
 os_version.distro, os_version.codename, os_version.major, os_version.minor,
 person.name
 FROM factory
-JOIN person ON factory.creator = person.person
+JOIN platform USING (platform)
 JOIN os_version USING (os_version)
 JOIN os USING (os)
-WHERE os_version.os_version = factory.os_version
+JOIN person ON factory.creator = person.person
 ORDER BY factory.name;
 
 SELECT factory.name, browser.manufacturer, browser.name,
-browser_version.major, browser_version.minor, browser.terminal, os.mobile
+browser_version.major, browser_version.minor, browser.terminal, os_version.mobile
 FROM factory_browser
 JOIN factory USING (factory)
 JOIN os_version USING (os_version)
@@ -38,3 +37,48 @@ JOIN request USING (request)
 JOIN website USING (website)
 JOIN factory USING (factory)
 JOIN browser USING (browser);
+
+SELECT DISTINCT browser.name
+FROM factory_browser
+JOIN factory USING (factory)
+JOIN os_version USING (os_version)
+JOIN os USING (os)
+JOIN browser_version USING (browser_version)
+JOIN browser USING (browser)
+WHERE os.name = 'Linux';
+
+SELECT DISTINCT browser.name
+FROM factory_browser
+JOIN factory USING (factory)
+JOIN os_version USING (os_version)
+JOIN os USING (os)
+JOIN browser_version USING (browser_version)
+JOIN browser USING (browser)
+WHERE os.name = 'Mac OS';
+
+SELECT DISTINCT browser.name
+FROM factory_browser
+JOIN factory USING (factory)
+JOIN os_version USING (os_version)
+JOIN os USING (os)
+JOIN browser_version USING (browser_version)
+JOIN browser USING (browser)
+WHERE os.name = 'Windows';
+
+SELECT DISTINCT browser.name
+FROM factory_browser
+JOIN factory USING (factory)
+JOIN os_version USING (os_version)
+JOIN os USING (os)
+JOIN browser_version USING (browser_version)
+JOIN browser USING (browser)
+WHERE browser.terminal;
+
+SELECT DISTINCT browser.name
+FROM factory_browser
+JOIN factory USING (factory)
+JOIN os_version USING (os_version)
+JOIN os USING (os)
+JOIN browser_version USING (browser_version)
+JOIN browser USING (browser)
+WHERE os_version.mobile;
