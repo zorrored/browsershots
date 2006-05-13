7e9be781c5bfc64db7cf7881dbdf02d405fb334c
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-05-13 23:18:27 +0000

    Redirect if the website address can be shown in the URL.

--- shotserver/lib/get/website.py
+++ shotserver/lib/get/website.py
@@ -25,16 +25,67 @@ __revision__ = '$Rev: 117 $'
 __date__ = '$Date: 2006-04-08 08:51:18 +0200 (Sat, 08 Apr 2006) $'
 __author__ = '$Author: johann $'
 
+import re
 from shotserver03.interface import xhtml
 from shotserver03 import database
+
+def request_is_numeric():
+    if len(req.info.options) != 1: return False
+    return req.info.options[0].isdigit()
+
+def request_numeric_to_int():
+    return int(req.info.options[0])
+
+def request_numeric_to_url():
+    request_int = request_numeric_to_int()
+    database.connect()
+    try:
+        cur.execute("SELECT url FROM website WHERE website = %s", request_int)
+        result = cur.fetchone()
+    finally:
+        database.disconnect()
+    if result is None:
+        return None
+    return result['url']
+
+suffix_match = re.compile(r'/(\w+)(\.\w{1,4})$').search
+def core_to_url(core):
+    if not core:
+        return None
+    elif suffix_match(core):
+        return 'http://%s' % core
+    else:
+        return 'http://%s/' % core
+
+simple_url_match = re.compile(r'^http://([\w\./]+)$').match
+def redirect():
+    """
+    Redirect if the website address can be shown in the URL.
+    """
+    if not request_is_numeric(): return False
+    url = request_numeric_to_url()
+    if url is None: return False
+    match = simple_url_match(url)
+    if match is None: return False
+    core = match.group(1)
+    if core_to_url(core) != url: return False
+    encoded = 'http://%s/website/%s/' % (req.info.uri.hostname, core)
+    req.headers_out['Location'] = encoded
+    return True
 
 def title():
     return "Website"
 
 def body():
-    assert len(req.info.options) == 1
-    website = int(req.info.options[0])
+    if request_is_numeric():
+        website = request_numeric_to_url()
+    else:
+        core = '/'.join(req.info.options)
+        website = core_to_url(core)
 
     xhtml.write_open_tag_line('div')
-    xhtml.write_tag_line('p', website)
+    if website is None:
+        xhtml.write_tag_line('p', "Unknown website.", _class="error")
+    else:
+        xhtml.write_tag_line('p', website)
     xhtml.write_close_tag_line('div')
