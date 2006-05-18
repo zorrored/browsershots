4fad1e48063b98a3aca98854104f09987bee1d8b
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-05-18 21:08:02 +0000

    Extensive error checking and reporting with redirect.

--- trunk/shotserver/lib/post/website.py
+++ trunk/shotserver/lib/post/website.py
@@ -25,8 +25,13 @@ __revision__ = '$Rev: 117 $'
 __date__ = '$Date: 2006-04-08 08:51:18 +0200 (Sat, 08 Apr 2006) $'
 __author__ = '$Author: johann $'
 
+import re, httplib, urllib, urlparse, socket
+from mod_python import util
 from shotserver03.interface import xhtml
 from shotserver03 import database
+
+class InvalidURLError(Exception):
+    pass
 
 def read_form(form):
     url = ''
@@ -55,8 +60,46 @@ def select_or_insert(url):
     finally:
         database.disconnect()
 
+def server_said(errornumber, errorstring, prefix = '', suffix = ''):
+    result = "The server said '%d %s'." % (errornumber, errorstring)
+    if prefix: result = prefix + ' ' + result
+    if suffix: result = result + ' ' + suffix
+    return result
+
+def error_redirect(**params):
+    location = '/?' + urllib.urlencode(params)
+    util.redirect(req, location)
+
+def sanity_check_url(url):
+    protocol, server, path, query, fragment = urlparse.urlsplit(url, 'http')
+    if protocol != 'http':
+        raise error_redirect(error = "Protocol %s is not supported." % protocol, url = url)
+    if not server:
+        raise error_redirect(error = "Malformed URL. Please check for typos.", url = url)
+    if not path:
+        raise error_redirect(error = "There should be a slash after the server name. Please try again.", url = url + '/')
+
+def test_head(url):
+    protocol, server, path, query, fragment = urlparse.urlsplit(url, 'http')
+    assert protocol == 'http'
+    connection = httplib.HTTPConnection(server)
+    try:
+        if query: path += '?' + query
+        connection.request('HEAD', path)
+    except socket.error, (errornumber, errorstring):
+        error_redirect(error = ' '.join(("Could not open web address.", errorstring + '.', "Please check for typos.")), url = url)
+    response = connection.getresponse()
+    if response.status == 302:
+        redirected = response.getheader('Location')
+        if fragment: redirected += '#' + fragment
+        error_redirect(error = server_said(response.status, response.reason, "Your request has been redirected.", "Please try again."),
+                       url = redirected)
+    if response.status != 200:
+        error_redirect(error = server_said(response.status, response.reason, "Unexpected server response."), url = url)
+
 def redirect():
     url = read_form(req.info.form)
+    sanity_check_url(url)
+    test_head(url)
     website = select_or_insert(url)
-    req.headers_out['Location'] = '/website/%d/' % website
-    return True
+    util.redirect(req, '/website/%d/' % website)
