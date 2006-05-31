730790d3312c4380899ecdf364649baae9ef5cd6
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-05-31 06:46:10 +0000

    Tuned feature select elements.

--- trunk/shotserver/lib/segments/features.py
+++ trunk/shotserver/lib/segments/features.py
@@ -49,21 +49,21 @@ def write():
     xhtml.write_open_tag_line('div', _class="float-left")
     xhtml.write_tag('b', "Screen resolution")
     xhtml.write_tag_line('br')
-    write_select('screen_resolution', "tiny=Tiny (640x480)|small=Small (800x600)|medium=Medium (1024x768)" +
+    write_select('width', "tiny=Tiny (640x480)|small=Small (800x600)|medium=Medium (1024x768)" +
                  "|large=Large (1280x1024)|huge=Huge (1600x1200)", 3)
     xhtml.write_close_tag_line('div')
 
     xhtml.write_open_tag_line('div', _class="float-left")
     xhtml.write_tag('b', "JavaScript")
     xhtml.write_tag_line('br')
-    write_select('javascript', "any=Don't Care|no=Disabled|yes=Enabled|1.3=Version 1.3|1.4=Version 1.4|" +
+    write_select('js', "dontcare=Don't Care|no=Disabled|yes=Enabled|1.3=Version 1.3|1.4=Version 1.4|" +
                  "1.5=Version 1.5|1.6=Version 1.6", 3)
     xhtml.write_close_tag_line('div')
 
     xhtml.write_open_tag_line('div', _class="float-left")
     xhtml.write_tag('b', "Macromedia Flash")
     xhtml.write_tag_line('br')
-    write_select('flash', "any=Don't Care|no=Not Installed|yes=Installed|4=Version 4|5=Version 5|6=Version 6" +
+    write_select('flash', "dontcare=Don't Care|no=Not Installed|yes=Installed|4=Version 4|5=Version 5|6=Version 6" +
                  "|7=Version 7|8=Version 8", 3)
     xhtml.write_close_tag_line('div')
 
@@ -71,27 +71,27 @@ def write():
     xhtml.write_open_tag_line('div', _class="float-left")
     xhtml.write_tag('b', "Maximum wait")
     xhtml.write_tag_line('br')
-    write_select('expire_minutes', "15=15 minutes|30=30 minutes|60=1 hour|120=2 hours|240=4 hours", 2)
+    write_select('expire', "15=15 minutes|30=30 minutes|60=1 hour|120=2 hours|240=4 hours", 2)
     xhtml.write_close_tag_line('div')
 
     xhtml.write_open_tag_line('div', _class="float-left")
     xhtml.write_tag('b', "Color depth")
     xhtml.write_tag_line('br')
-    write_select('bits_per_pixel', "any=Don't Care|4=4 Bits (16 Colors)|8=8 Bits (256 Colors)|" +
+    write_select('bpp', "dontcare=Don't Care|4=4 Bits (16 Colors)|8=8 Bits (256 Colors)|" +
                  "16=16 Bits (High Color)|24=24 Bits (True Color)")
     xhtml.write_close_tag_line('div')
 
     xhtml.write_open_tag_line('div', _class="float-left")
     xhtml.write_tag('b', "Java")
     xhtml.write_tag_line('br')
-    write_select('java', "any=Don't Care|no=Not Installed|yes=Installed|blackdown=Blackdown|kaffe=Kaffe|sun=Sun Java" +
+    write_select('java', "dontcare=Don't Care|no=Not Installed|yes=Installed|blackdown=Blackdown|kaffe=Kaffe|sun=Sun Java" +
                  "|sun_1.2=Sun Java 1.2|sun_1.3=Sun Java 1.3|sun_1.4=Sun Java 1.4|sun_5.0=Sun Java 5.0")
     xhtml.write_close_tag_line('div')
 
     xhtml.write_open_tag_line('div', _class="float-left")
     xhtml.write_tag('b', "Media plugins")
     xhtml.write_tag_line('br')
-    write_select('media', "any=Don't Care|quicktime=Apple Quicktime|wmp=Windows Media Player|svg=SVG|pdf=PDF")
+    write_select('media', "dontcare=Don't Care|quicktime=Apple Quicktime|wmp=Windows Media Player|svg=SVG|pdf=PDF")
     xhtml.write_close_tag_line('div')
 
     xhtml.write_tag_line('div', '', _class="clear")
