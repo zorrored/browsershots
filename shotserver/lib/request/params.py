8bab8d245110c1c6ed40c2598f227c6959344f26
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-06-01 05:58:14 +0000

    Simple object to hold request parameters.

---
+++ shotserver/lib/request/params.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# browsershots.org
+# Copyright (C) 2006 Johann C. Rocholl <johann@browsershots.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+# MA 02111-1307, USA.
+
+"""
+Application parameters from a HTTP request.
+"""
+
+__revision__ = '$Rev: 279 $'
+__date__ = '$Date: 2006-05-31 23:30:07 +0200 (Wed, 31 May 2006) $'
+__author__ = '$Author: johann $'
+
+class Params:
+    """Store request parameters."""
+    pass
