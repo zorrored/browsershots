579b0465e511c50db68d2a96406143d9d227ae22
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-06-12 15:26:52 +0000

    Break long line into parts.

--- trunk/shotserver/lib/segments/languages.py
+++ trunk/shotserver/lib/segments/languages.py
@@ -30,8 +30,10 @@ def write():
     xhtml.write_open_tag_line('form', action="")
     xhtml.write_open_tag_line('div', _id="languages", _class="float-right")
 
-    xhtml.write_open_tag_line('select', _id="langsel",
-        onchange="document.location.href='/intl/'+this.form.langsel.options[this.form.langsel.options.selectedIndex].value+document.location.pathname")
+    parts = ("document.location.href='/intl/'",
+             "this.form.langsel.options[this.form.langsel.options.selectedIndex].value",
+             "document.location.pathname")
+    xhtml.write_open_tag_line('select', _id="langsel", onchange='+'.join(parts))
     xhtml.write_tag_line('option', 'English', value="en")
     xhtml.write_tag_line('option', 'English (Canada)', value="en-CA")
     xhtml.write_tag_line('option', 'Deutsch', value="de")
