1d29d98d493085167bc88e783a41542ba840b5a7
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-08-13 15:47:56 +0000

    Links to feedvalidator.org for RSS 2.0 and Atom.

--- trunk/pyblosxom/plugins/feedlinks.py
+++ trunk/pyblosxom/plugins/feedlinks.py
@@ -16,12 +16,17 @@ def cb_prepare(args):
     if path:
         path = '/' + path
 
+    base_url = conf['base_url']
     feedlinks = []
-    feedlinks.append('<link rel="alternate" type="application/rss+xml"' +
-                     ' href="%s%s/index.rss20" />' % (conf['base_url'], path))
-    feedlinks.append('<link rel="alternate" type="application/atom+xml"' +
-                     ' href="%s%s/index.atom" />' % (conf['base_url'], path))
+    feedlinks.append('<link rel="alternate" type="application/rss+xml" href="%s%s/index.rss20" />' % (base_url, path))
+    feedlinks.append('<link rel="alternate" type="application/atom+xml" href="%s%s/index.atom" />' % (base_url, path))
     data["feedlinks"] = '\n'.join(feedlinks)
+
+    base_url = base_url.replace(':', '%3A')
+    feedvalidators = []
+    feedvalidators.append('<a href="http://feedvalidator.org/check.cgi?url=%s%s/index.rss20"><img src="/static/rss20.png" alt="Valid RSS 2.0" /></a>' % (base_url, path))
+    feedvalidators.append('<a href="http://feedvalidator.org/check.cgi?url=%s%s/index.atom"><img src="/static/atom.png" alt="Valid Atom" /></a>' % (base_url, path))
+    data["feedvalidators"] = '<br />\n'.join(feedvalidators)
 
 
 def cb_end(args):
@@ -29,3 +34,5 @@ def cb_end(args):
     data = request.getData()
     if "feedlinks" in data:
         del data["feedlinks"]
+    if "feedvalidators" in data:
+        del data["feedvalidators"]
