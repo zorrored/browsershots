4cab30d17435338a5dd7be380c860ca64749bf8d
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-08-17 23:14:21 +0000

    List all wiki pages.

--- trunk/trac/plugins/SimpleBlogPlugin/simpleblog/__init__.py
+++ trunk/trac/plugins/SimpleBlogPlugin/simpleblog/__init__.py
@@ -1,6 +1,7 @@
 from trac.core import *
 from trac.web import IRequestHandler
 from trac.web.chrome import INavigationContributor
+from trac.wiki.api import WikiSystem
 from trac.util import Markup, format_date, format_datetime
 
 class SimpleBlogPlugin(Component):
@@ -14,10 +15,12 @@ class SimpleBlogPlugin(Component):
             '<a href="%s">Blog</a>' % self.env.href.blog())
 
     # IRequestHandler methods
-    anonymous_request = True
     def match_request(self, req):
         return req.path_info == '/blog'
     def process_request(self, req):
         req.hdf['wiki.action'] = 'view'
-        req.hdf['wiki.page_html'] = 'Hallo Welt!'
+        page_html = []
+        for page in WikiSystem(self.env).get_pages():
+            page_html.append(repr(page))
+        req.hdf['wiki.page_html'] = '\n'.join(page_html)
         return 'wiki.cs', None
