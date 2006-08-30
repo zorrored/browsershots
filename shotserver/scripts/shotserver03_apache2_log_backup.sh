3450e0616d66949b8ce15f4341ad8d4d72a8c834
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-08-30 09:21:17 +0000

    Moved log_dates.py from logrotate.d/apache2 to backup cronjob.

--- shotserver/scripts/shotserver03_apache2_log_backup.sh
+++ shotserver/scripts/shotserver03_apache2_log_backup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 LOGS=/var/log/apache2/
 BACKUP=/backup/`hostname`$LOGS
-rsync -trv --exclude "*.log" --exclude "*.log.?" $LOGS $BACKUP
+shotserver03_log_dates.py $LOGS*.log.?.gz $LOGS*/*.log.?.gz
+rsync -tr --exclude "*.log" --exclude "*.log.?" $LOGS $BACKUP
