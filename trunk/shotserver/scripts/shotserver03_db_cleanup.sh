7bc9ee15f71ee6016b8e7917a1e0404917871170
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-08-31 11:31:13 +0000

    Delete old requests that were never locked and request_groups without requests.

--- trunk/shotserver/scripts/shotserver03_db_cleanup.sh
+++ trunk/shotserver/scripts/shotserver03_db_cleanup.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 echo "DELETE FROM nonce WHERE created < NOW()-'24:00'::interval" | su - www-data psql shotserver03
+echo "DELETE FROM request WHERE request IN (SELECT request FROM request JOIN request_group USING (request_group) WHERE created < NOW()-'24:00'::interval AND locked IS NULL)" | su - www-data psql shotserver03
+echo "DELETE FROM request_group WHERE NOT EXISTS (SELECT request FROM request WHERE request.request_group = request_group.request_group)" | su - www-data psql shotserver03
