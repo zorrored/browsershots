59735b914b48aba2acef487430bba375939d1058
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-09-16 23:49:52 +0000

    Fixed red headline links in MSIE.

--- trunk/trac/plugins/SimpleBlogPlugin/simpleblog/templates/blog.cs
+++ trunk/trac/plugins/SimpleBlogPlugin/simpleblog/templates/blog.cs
@@ -5,7 +5,7 @@
 <div id="content" class="wikipage">
 <?cs
 each:event = blog.events ?>
-<h1><a href="<?cs var:event.href ?>" style="border-bottom-style: none;"><?cs var:event.title ?></a></h1>
+<h1><a href="<?cs var:event.href ?>" style="border-bottom-style: none; color: black;"><?cs var:event.title ?></a></h1>
 <p style="font-size: smaller; color: gray; margin: 0 0 0 -18px;"><?cs var:event.date ?> | <?cs var:event.author ?> | <a href="<?cs var:event.href ?>" title="permalink" style="border-bottom-style: none;">#</a><?cs
 if:event.comments ?> | <?cs var:event.comments ?> comments<?cs /if ?>
 <?cs var:event.description ?>
