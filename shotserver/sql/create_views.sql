341060a53815ad11a31a10f32ab40fc4f4060f5e
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-09-18 19:22:53 +0000

    Removed obsolete SQL files.

--- shotserver/sql/create_views.sql
+++
@@ -1,6 +0,0 @@
-CREATE VIEW browser_stats AS
-SELECT browser_group.name, COUNT(*) AS queued
-FROM request
-JOIN browser_group USING (browser_group)
-WHERE screenshot IS NULL
-GROUP BY browser_group.name;
