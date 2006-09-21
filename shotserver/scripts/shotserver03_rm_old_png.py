3646db6a9c5ecf4958cdbf13ab8a5e02426c86b8
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-09-21 21:57:41 +0000

    Count deleted objects rather than printing each filename and database hashkey.

--- shotserver/scripts/shotserver03_rm_old_png.py
+++ shotserver/scripts/shotserver03_rm_old_png.py
@@ -12,27 +12,30 @@ from shotserver03 import database as db
 
 png_path = '/var/www/browsershots.org/png'
 subdirs = '140 180 240 450 full'.split()
-timeout = 60 # days
+timeout = 50 # days
 now = time.time()
 expire = now - timeout * 24 * 3600
 
 
-def database_delete(filename):
+def database_delete(filename, counter):
     hashkey = filename[:32]
     cur.execute('DELETE FROM screenshot WHERE hashkey = %s', (hashkey, ))
     if cur.rowcount:
-        print hashkey, 'deleted from database'
+        counter['rows'] += 1
+        # print hashkey, 'deleted from database'
 
 
-def files_delete(filename):
+def files_delete(filename, counter):
     for subdir in subdirs:
         path = os.path.join(png_path, subdir, prefix, filename)
         if os.path.exists(path):
             os.unlink(path)
-            print path, 'deleted'
+            counter['files'] += 1
+            # print path, 'deleted'
 
 
 def find_expired(filelist):
+    counter = {'files': 0, 'rows': 0}
     for filename in filelist:
         prefix = filename[:2]
         expired = True
@@ -48,15 +51,15 @@ def find_expired(filelist):
             # else:
             #     print path, 'not found'
         if expired:
-            database_delete(filename)
-            files_delete(filename)
+            database_delete(filename, counter)
+            files_delete(filename, counter)
+    return counter
 
 
 db.connect()
 try:
     for index in range(256):
         prefix = '%02x' % index
-        print 'checking', prefix
         filelist = ()
         for subdir in subdirs:
             path = os.path.join(png_path, subdir, prefix)
@@ -64,6 +67,9 @@ try:
                 candidate = os.listdir(path)
                 if len(candidate) > len(filelist):
                     filelist = candidate
-        find_expired(filelist)
+        counter = find_expired(filelist)
+        if counter['rows'] or counter['files']:
+            print '%s: deleted %s database rows and %s files' % (
+                prefix, counter['rows'], counter['files'])
 finally:
     db.disconnect()
