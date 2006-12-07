e922cd8e71e04d464d039f584afeca34195bdf7f
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-12-07 02:15:41 +0000

    Removed obsolete database test script.

--- trunk/shotserver/scripts/shotserver03_db_test_sql.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cat sql/create_tables.sql | sudo su - www-data psql shotserver03 2>&1 | grep -v NOTICE
-cat sql/create_views.sql | sudo su - www-data psql shotserver03 2>&1 | grep -v NOTICE
-cat sql/test_insert.sql | sudo su - www-data psql shotserver03 2>&1 | grep -v NOTICE | grep -v INSERT
-# cat sql/test_select.sql | sudo su - www-data psql shotserver03
