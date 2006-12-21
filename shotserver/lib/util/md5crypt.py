231e0400781b36ba4606ea1a4d10288f230fee48
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-12-21 00:36:10 +0000

    Some cleanup and formatting.

--- shotserver/lib/util/md5crypt.py
+++ shotserver/lib/util/md5crypt.py
@@ -1,28 +1,27 @@
 # Based on FreeBSD src/lib/libcrypt/crypt.c 1.2
-# http://www.freebsd.org/cgi/cvsweb.cgi/~checkout~/src/lib/libcrypt/crypt.c?rev=1.2&content-type=text/plain
+# http://www.freebsd.org/cgi/cvsweb.cgi/src/lib/libcrypt/crypt.c?rev=1.2
 
 # Original license:
-# * "THE BEER-WARE LICENSE" (Revision 42):
-# * <phk@login.dknet.dk> wrote this file.  As long as you retain this notice you
-# * can do whatever you want with this stuff. If we meet some day, and you think
-# * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
-
-# This port adds no further stipulations.  I forfeit any copyright interest.
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <phk@login.dknet.dk> wrote this file.  As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
 
 import md5
 
-def md5crypt(password, salt, magic='$1$'):
-    # /* The password first, since that is what is most unknown */ /* Then our magic string */ /* Then the raw salt */
-    m = md5.new()
-    m.update(password + magic + salt)
 
-    # /* Then just as many characters of the MD5(pw,salt,pw) */
+def md5crypt(password, salt, magic='$1$'):
+    m = md5.new()
+    m.update(password) # The password first, since that is what is most unknown
+    m.update(magic)    # Then our magic string
+    m.update(salt)     # Then the raw salt
+
+    # Then just as many characters of the MD5(pw,salt,pw)
     mixin = md5.md5(password + salt + password).digest()
     for i in range(0, len(password)):
         m.update(mixin[i % 16])
 
-    # /* Then something really weird... */
-    # Also really broken, as far as I can tell.  -m
+    # Then something really weird...
     i = len(password)
     while i:
         if i & 1:
@@ -30,40 +29,33 @@ def md5crypt(password, salt, magic='$1$'):
         else:
             m.update(password[0])
         i >>= 1
-
     final = m.digest()
 
-    # /* and now, just to make sure things don't run too fast */
+    # And now, just to make sure things don't run too fast
     for i in range(1000):
         m2 = md5.md5()
         if i & 1:
             m2.update(password)
         else:
             m2.update(final)
-
         if i % 3:
             m2.update(salt)
-
         if i % 7:
             m2.update(password)
-
         if i & 1:
             m2.update(final)
         else:
             m2.update(password)
-
         final = m2.digest()
 
     # This is the bit that uses to64() in the original code.
-
     itoa64 = './0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-
     rearranged = ''
-    for a, b, c in ((0, 6, 12), (1, 7, 13), (2, 8, 14), (3, 9, 15), (4, 10, 5)):
+    for a, b, c in ((0, 6, 12), (1, 7, 13), (2, 8, 14),
+                    (3, 9, 15), (4, 10, 5)):
         v = ord(final[a]) << 16 | ord(final[b]) << 8 | ord(final[c])
         for i in range(4):
             rearranged += itoa64[v & 0x3f]; v >>= 6
-
     v = ord(final[11])
     for i in range(2):
         rearranged += itoa64[v & 0x3f]; v >>= 6
@@ -83,8 +75,7 @@ if __name__ == '__main__':
         ('____fifteen____', '$1$s9lUWACI$Kk1jtIVVdmT01p0z3b/hw1'),
         ('____sixteen_____', '$1$dL3xbVZI$kkgqhCanLdxODGq14g/tW1'),
         ('____seventeen____', '$1$NaH5na7J$j7y8Iss0hcRbu3kzoJs5V.'),
-        ('__________thirty-three___________', '$1$HO7Q6vzJ$yGwp2wbL5D7eOVzOmxpsy.'),
-        ('apache', '$apr1$J.w5a/..$IW9y6DR0oO/ADuhlMF5/X1')
+        ('apache', '$apr1$J.w5a/..$IW9y6DR0oO/ADuhlMF5/X1'),
     )
 
     for clearpw, hashpw in test_cases:
