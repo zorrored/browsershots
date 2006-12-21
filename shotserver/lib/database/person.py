e5e5b978a63a0fdc8c00381bd65160b4b1c85f1a
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-12-21 01:37:11 +0000

    Web-based user registration.

---
+++ shotserver/lib/database/person.py
@@ -0,0 +1,60 @@
+# browsershots.org
+# Copyright (C) 2006 Johann C. Rocholl <johann@browsershots.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+# MA 02111-1307, USA.
+
+"""
+Database interface for person table.
+"""
+
+__revision__ = '$Rev$'
+__date__ = '$Date$'
+__author__ = '$Author$'
+
+import md5
+from shotserver03.util import md5nonce
+from shotserver03.util import md5crypt
+
+
+def select_username(serial):
+    """
+    Get the username from the database.
+    """
+    cur.execute("SELECT username FROM person WHERE person=%s", (serial, ))
+    result = cur.fetchone()
+    if result is None:
+        return None
+    return result[0]
+
+
+def insert(fields):
+    """
+    Insert a new user into the database.
+    """
+    salt = md5nonce.random_md5()
+    password = fields['password']
+    fields['salt'] = salt[:4]
+    fields['password'] = md5.md5(salt + password).hexdigest()
+    fields['htpasswd'] = md5crypt.md5crypt(password, salt[-8:], '$apr1$')
+    cur.execute("""\
+INSERT INTO person
+(name, email, nickname, salt, password, htpasswd)
+VALUES (%(fullname)s, %(email)s, %(username)s, %(salt)s,
+        %(password)s, %(htpasswd)s)
+""", fields)
+    cur.execute("SELECT lastval()")
+    row = cur.fetchone()
+    return row[0]
