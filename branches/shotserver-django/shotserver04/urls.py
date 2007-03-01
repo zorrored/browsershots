4d127d290290a46c597a0118524b5ee02cca640d
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-03-01 14:02:47 +0000

    django-admin.py startproject shotserver04

---
+++ branches/shotserver-django/shotserver04/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^shotserver04/', include('shotserver04.foo.urls')),
+
+    # Uncomment this for admin:
+#     (r'^admin/', include('django.contrib.admin.urls')),
+)
