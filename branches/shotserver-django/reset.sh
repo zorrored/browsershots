f1452ebbc3fc8852ca849534e892ec7ee9e20d67
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-03-02 21:14:17 +0000

    Moved extra scripts out of Django tree.

--- branches/shotserver-django/reset.sh
+++ branches/shotserver-django/reset.sh
@@ -5,10 +5,10 @@
 # Run this script as the user www-data (or your database user).
 
 # Drop all tables (note reverse app ordering).
-python manage.py sqlclear screenshots requests browsers factories | psql shotserver04
+python shotserver04/manage.py sqlclear screenshots requests browsers factories | psql shotserver04
 
 # Create all tables from Django models.
-python manage.py syncdb factories browsers requests screenshots
+python shotserver04/manage.py syncdb factories browsers requests screenshots
 
 # Load some database content from demo.sql for testing.
 psql shotserver04 < demo.sql
