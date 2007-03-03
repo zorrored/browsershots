378d25ec7a11a58c541318539e0f4cc0acdad489
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-03-03 11:24:09 +0000

    Cascade (for changes in constraints etc).

--- branches/shotserver-django/reset.sh
+++ branches/shotserver-django/reset.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
+
 #######################################################
 # WARNING: THIS WILL DROP ALL TABLES IN YOUR DATABASE #
 #######################################################
-# Run this script as the user www-data (or your database user).
 
-# Drop all tables (note reverse app ordering).
-python shotserver04/manage.py sqlclear screenshots requests browsers factories | psql shotserver04
+# Run this script as the user www-data (or your database user) after
+# changing the database models. If you want to keep your data, use
+# pg_dump before you run this script, then reload from the dump file.
+
+APPS="factories browsers requests screenshots"
+
+# Drop all tables, with cascade.
+python shotserver04/manage.py sqlclear $APPS | \
+grep -v CONSTRAINT | sed 's/";/" CASCADE;/' | \
+psql shotserver04
 
 # Create all tables from Django models.
-python shotserver04/manage.py syncdb factories browsers requests screenshots
-
-# Load some database content from demo.sql for testing.
-psql shotserver04 < demo.sql
+python shotserver04/manage.py syncdb $APPS
