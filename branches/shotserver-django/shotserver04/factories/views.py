3ddce58d1ca7c01d65541627419fc11bd3e09b94
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-03-05 16:18:02 +0000

    Using generic view for factory list, with pagination.

--- branches/shotserver-django/shotserver04/factories/views.py
+++ branches/shotserver-django/shotserver04/factories/views.py
@@ -3,15 +3,7 @@ from shotserver04.factories.models import Factory
 from shotserver04.browsers.models import Browser
 
 
-def index(request):
-    factory_list = Factory.objects.all()
-    return render_to_response('factories/index.html',
-                              {'factory_list': factory_list})
-
-
-def details(request, factory_name):
+def factory_detail(request, factory_name):
     factory = Factory.objects.get(name=factory_name)
-    return render_to_response(
-        'factories/details.html',
-        {'factory_name': factory_name,
-         'browser_list': Browser.objects.filter(factory=factory.id)})
+    browser_list = Browser.objects.filter(factory=factory.id)
+    return render_to_response('factories/factory_detail.html', locals())
