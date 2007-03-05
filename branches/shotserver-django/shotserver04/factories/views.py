f885ed0302e953fa2d8b4d82e531118fc454c86b
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-03-05 16:26:42 +0000

    Raise Http404 if factory name does not exist.

--- branches/shotserver-django/shotserver04/factories/views.py
+++ branches/shotserver-django/shotserver04/factories/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render_to_response
+from django.http import Http404
 from shotserver04.factories.models import Factory
 from shotserver04.browsers.models import Browser
 
 
 def factory_detail(request, factory_name):
-    factory = Factory.objects.get(name=factory_name)
+    try:
+        factory = Factory.objects.get(name=factory_name)
+    except Factory.DoesNotExist:
+        raise Http404
     browser_list = Browser.objects.filter(factory=factory.id)
     return render_to_response('factories/factory_detail.html', locals())
