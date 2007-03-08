aaa3e98ee1efebe88390187a3c623824a15e137a
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-03-08 17:27:04 +0000

    Show command in admin list.

--- branches/shotserver-django/shotserver04/browsers/models.py
+++ branches/shotserver-django/shotserver04/browsers/models.py
@@ -56,6 +56,6 @@ class Browser(models.Model):
             'disabled',
             )}),
             )
-        list_display = ('factory', 'browser_group', 'version')
+        list_display = ('factory', 'browser_group', 'version', 'command')
         list_display_links = ('browser_group', 'version')
         list_filter = ('factory', )
