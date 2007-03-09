684d606a4745f8f29895a38ae0442898d06d671f
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-03-09 08:24:25 +0000

    Sortable factory list.

--- branches/shotserver-django/shotserver04/factories/urls.py
+++ branches/shotserver-django/shotserver04/factories/urls.py
@@ -3,15 +3,7 @@ from django.views.generic import list_detail
 from shotserver04.factories.models import Factory
 from shotserver04.browsers.models import Browser
 
-factory_list_info = {
-    'queryset': Factory.objects.all(),
-    'allow_empty': True,
-    'paginate_by': 10,
-    }
-
-urlpatterns = patterns('',
-    (r'^$', list_detail.object_list, factory_list_info),
-    (r'^page(?P<page>\d+)/$', list_detail.object_list, factory_list_info),
-    (r'^(?P<factory_name>\S+)/$',
-     'shotserver04.factories.views.factory_detail'),
+urlpatterns = patterns('shotserver04.factories.views',
+    (r'^$', 'factory_list'),
+    (r'^(?P<factory_name>\S+)/$', 'factory_detail'),
 )
--- branches/shotserver-django/shotserver04/factories/views.py
+++ branches/shotserver-django/shotserver04/factories/views.py
@@ -3,6 +3,46 @@ from django.http import Http404
 from shotserver04.factories.models import Factory
 from shotserver04.browsers.models import Browser
 
+FACTORY_LIST_HEADERS = (
+    ('name', "Factory"),
+    ('operating_system', "Operating<br />system"),
+    ('architecture', "CPU"),
+    ('last_poll', "Last<br />poll"),
+    ('last_upload', "Last<br />upload"),
+    ('uploads_per_hour', "Uploads<br />per hour"),
+    ('uploads_per_day', "Uploads<br />per day"),
+)
+
+
+def factory_list(request):
+    order = request.GET.get('order', '')
+    order_column = order
+    descending = order.startswith('-')
+    if descending:
+        order_column = order[1:]
+    if not order_column in [x[0] for x in FACTORY_LIST_HEADERS]:
+        order = 'name'
+        order_column = 'name'
+        descending = False
+    header_list = []
+    for column, text in FACTORY_LIST_HEADERS:
+        class_attrib = ''
+        if column == order_column:
+            found = True
+            if descending:
+                class_attrib = ' class="sorted descending"'
+                url = '?order=' + column
+            else:
+                class_attrib = ' class="sorted ascending"'
+                url = '?order=-' + column
+        else:
+            url = '?order=' + column
+        header_list.append({'text': text,
+                            'url': url,
+                            'class_attrib': class_attrib})
+    factory_list = list(Factory.objects.order_by(order))
+    return render_to_response('factories/factory_list.html', locals())
+
 
 def factory_detail(request, factory_name):
     try:
