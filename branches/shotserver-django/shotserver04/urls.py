eca4b539a4a1e68648bbd3c002ed6ede919f8971
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-03-09 16:06:13 +0000

    Automatic URLs for installed apps.

--- branches/shotserver-django/shotserver04/urls.py
+++ branches/shotserver-django/shotserver04/urls.py
@@ -1,6 +1,18 @@
 from django.conf.urls.defaults import *
+from shotserver04 import settings
+
+
+def load_app_patterns(prefix, ignore=()):
+    pairs = []
+    for app in settings.INSTALLED_APPS:
+        if app.startswith(prefix):
+            segment = app.split('.')[-1]
+            if segment not in ignore:
+                pairs.append((r'^%s/' % segment, include(app + '.urls')))
+    return pairs
+
 
 urlpatterns = patterns('',
-    (r'^factories/', include('shotserver04.factories.urls')),
     (r'^admin/', include('django.contrib.admin.urls')),
-)
+    *load_app_patterns('shotserver04.', ignore=['common'])
+    )
