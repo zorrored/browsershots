f0ff97f464288ba98a91f11b31904c9b7aec214e
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-03-10 16:48:21 +0000

    More tests for valid and invalid URLs.

--- branches/shotserver-django/shotserver04/websites/sql/website.sql
+++ branches/shotserver-django/shotserver04/websites/sql/website.sql
@@ -1,3 +1,3 @@
 ALTER TABLE websites_website
 ADD CONSTRAINT websites_website_url_check
-CHECK (url ~ '^https?://[a-z0-9_\.\-]*/');
+CHECK (url ~ '^https?://[a-z0-9_\\.\\-]+\\.[a-z0-9]+/\\S*$');
--- branches/shotserver-django/shotserver04/websites/tests.py
+++ branches/shotserver-django/shotserver04/websites/tests.py
@@ -22,25 +22,58 @@ class WebsitesTestCase(TestCase):
         self.assertEqual(len(Website.objects.filter(
             url__contains='browsershots.org')), 1)
 
-    def testEmptyURL(self):
-        self.website.url = ''
-        self.assertRaises(IntegrityError, self.website.save)
-        transaction.rollback()
-
-    def testInvalidURL(self):
-        self.website.url = '-'
-        self.assertRaises(IntegrityError, self.website.save)
-        transaction.rollback()
-
     def testCreateDuplicate(self):
         self.assertRaises(IntegrityError, Website.objects.create,
                           url='http://browsershots.org/')
         transaction.rollback()
 
-    def testCreateEmpty(self):
-        self.assertRaises(IntegrityError, Website.objects.create, url='')
-        transaction.rollback()
 
-    def testCreateInvalid(self):
-        self.assertRaises(IntegrityError, Website.objects.create, url='-')
-        transaction.rollback()
+class UrlTestCase(TestCase):
+
+    def assertInvalid(self, url):
+        try:
+            website = Website.objects.create(url=url)
+            website.delete()
+            self.fail("invalid URL did not raise IntegrityError: '%s'" % url)
+        except IntegrityError:
+            transaction.rollback()
+
+    def assertValid(self, url):
+        self.assertEqual(Website.objects.filter(url=url).count(), 0)
+        try:
+            website = Website.objects.create(url=url)
+            website.save()
+            transaction.commit()
+        except IntegrityError:
+            self.fail("valid URL raised IntegrityError: '%s'" % url)
+        self.assertEqual(Website.objects.filter(url=url).count(), 1)
+        website.delete()
+        transaction.commit()
+        self.assertEqual(Website.objects.filter(url=url).count(), 0)
+
+    def testInvalidA(self): self.assertInvalid('')
+    def testInvalidB(self): self.assertInvalid(' ')
+    def testInvalidC(self): self.assertInvalid('h')
+    def testInvalidD(self): self.assertInvalid('http')
+    def testInvalidE(self): self.assertInvalid('http:')
+    def testInvalidF(self): self.assertInvalid('http:/')
+    def testInvalidG(self): self.assertInvalid('http://')
+    def testInvalidH(self): self.assertInvalid('http:///')
+    def testInvalidI(self): self.assertInvalid('http://browsershots/')
+    def testInvalidJ(self): self.assertInvalid('htp://browsershots.org/')
+    def testInvalidK(self): self.assertInvalid('http//browsershots.org/')
+    def testInvalidL(self): self.assertInvalid('http:/browsershots.org/')
+    def testInvalidM(self): self.assertInvalid('http://browsershots.org')
+    def testInvalidN(self): self.assertInvalid('http://browsershots.org/ ')
+    def testInvalidO(self): self.assertInvalid(' http://browsershots.org/')
+
+    def testValidA(self):
+        self.assertValid('http://browsershots.org/')
+    def testValidB(self):
+        self.assertValid('http://browsershots.org/http://example.com/')
+    def testValidC(self):
+        self.assertValid('http://browsershots.org/robots.txt')
+    def testValidD(self):
+        self.assertValid('http://browsershots.org/?url=http://example.com/')
+    def testValidE(self):
+        self.assertValid('https://browsershots.org/')
