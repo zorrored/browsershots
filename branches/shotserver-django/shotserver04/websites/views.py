bebbffb5ec8f4f037e38c1d717756a65c1345c2d
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-03-15 17:42:42 +0000

    Show a list of websites, with search box.

--- branches/shotserver-django/shotserver04/websites/views.py
+++ branches/shotserver-django/shotserver04/websites/views.py
@@ -1,5 +1,21 @@
+from django.db import connection
+from django.http import Http404
 from django.shortcuts import render_to_response
 from shotserver04.websites.models import Website
+
+
+def website_list(request):
+    website_list = Website.objects
+    search_query = request.GET.get('q', '')
+    for search in search_query.split():
+        if search.islower(): # Case insensitive search
+            website_list = website_list.filter(url__icontains=search)
+        else: # Case sensitive search if mixed case in query
+            website_list = website_list.filter(url__contains=search)
+    website_list = website_list.order_by('-submitted')
+    website_list = website_list[:100]
+    query_list = connection.queries
+    return render_to_response('websites/website_list.html', locals())
 
 
 def website_detail(request, website_url):
@@ -7,4 +23,5 @@ def website_detail(request, website_url):
         website = Website.objects.get(url=website_url)
     except Website.DoesNotExist:
         website = None
+    query_list = connection.queries
     return render_to_response('websites/website_detail.html', locals())
