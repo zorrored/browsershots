f168072ae1dc56984947ec60ccc9338d2fc5cee8
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-03-23 16:34:39 +0000

    Show active browsers for Linux, Windows, Mac.

--- branches/shotserver-django/shotserver04/common/views.py
+++ branches/shotserver-django/shotserver04/common/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render_to_response
+from django.db import connection
 from django import newforms as forms
+from django.shortcuts import render_to_response
 from shotserver04.factories.models import Factory
 from shotserver04.browsers.models import Browser
 
@@ -55,26 +56,59 @@ class OptionsForm(forms.Form):
 
 class BrowserForm(forms.BaseForm):
 
-    def __init__(self, data=None):
-        forms.BaseForm.__init__(self)
-        self.data = data
-        for factory in Factory.objects.filter(uploads_per_hour__gt=0):
-            self.fields[factory.name] = forms.BooleanField()
-
     errors = {}
     base_fields = forms.forms.SortedDictFromList()
 
-    #def __unicode__(self):
-    #    return u'\n'.join(self.fields)
+    def __init__(self, os, data=None):
+        forms.BaseForm.__init__(self)
+        self.data = data
+        self.parts = 1
+        browsers = Browser.objects.select_related()
+        os_browsers = browsers.filter(
+            factory__operating_system__operating_system_group__name=os,
+            disabled=False)
+        active_browsers = os_browsers.extra(
+            where=['"factories_factory"."last_poll" > NOW() - %s::interval'],
+            params=['31d'])
+        for browser in active_browsers:
+            name = browser.browser_group.name
+            if browser.major is not None:
+                name += ' ' + str(browser.major)
+                if browser.minor is not None:
+                    name += '.' + str(browser.minor)
+            code = name.lower().replace(' ', '-').replace('.', '-')
+            self.fields[code] = forms.BooleanField(label=name)
+
+    def __unicode__(self):
+        fields = list(self.fields)
+        fields_per_part = (len(fields) + self.parts - 1) / self.parts
+        print len(fields), self.parts, fields_per_part
+        output = []
+        for part in range(self.parts):
+            output.append('<div style="width:10em;float:left">')
+            for index in range(fields_per_part):
+                if not fields:
+                    break
+                field = fields.pop(0)
+                output.append(unicode(self[field]) + ' ' +
+                              self[field].label + '<br />\n')
+            output.append('</div>')
+        return u'\n'.join(output)
 
 
 def start(request):
     if request.POST:
         url_form = URLForm(request.POST)
         options_form = OptionsForm(request.POST)
-        browser_form = BrowserForm(request.POST)
+        linux_browsers = BrowserForm('Linux', request.POST)
+        windows_browsers = BrowserForm('Windows', request.POST)
+        mac_browsers = BrowserForm('Mac OS', request.POST)
     else:
         url_form = URLForm()
         options_form = OptionsForm()
-        browser_form = BrowserForm()
+        linux_browsers = BrowserForm('Linux')
+        windows_browsers = BrowserForm('Windows')
+        mac_browsers = BrowserForm('Mac OS')
+    linux_browsers.parts = 4
+    query_list = connection.queries
     return render_to_response('start.html', locals())
