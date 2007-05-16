2f4c63b9ef121d0f2a20e01bea5cda8d493de3bc
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-05-16 20:32:31 +0000

    Auto-register functions from xmlrpc modules in installed apps.

--- branches/shotserver-django/shotserver04/xmlrpc/views.py
+++ branches/shotserver-django/shotserver04/xmlrpc/views.py
@@ -1,9 +1,7 @@
+import types
 from SimpleXMLRPCServer import SimpleXMLRPCDispatcher
 from django.http import HttpResponse
-
-# dispatcher = SimpleXMLRPCDispatcher(allow_none=False, encoding=None)
-dispatcher = SimpleXMLRPCDispatcher()
-dispatcher.register_introspection_functions()
+from shotserver04 import settings
 
 
 def xmlrpc(request):
@@ -12,3 +10,16 @@ def xmlrpc(request):
         response.write(dispatcher._marshaled_dispatch(request.raw_post_data))
     response['Content-length'] = str(len(response.content))
     return response
+
+
+dispatcher = SimpleXMLRPCDispatcher()
+dispatcher.register_introspection_functions()
+for app in settings.INSTALLED_APPS:
+    try:
+        module = __import__(app + '.xmlrpc', globals(), locals(), ['xmlrpc'])
+    except ImportError:
+        continue
+    for name, item in module.__dict__.items():
+        if isinstance(item, types.FunctionType):
+            function_name = '%s.%s' % (app.split('.')[-1], name)
+            dispatcher.register_function(item, function_name)
