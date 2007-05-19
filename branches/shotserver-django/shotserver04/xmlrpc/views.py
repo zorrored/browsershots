5616104bbea859a27a9f01ffc6f42fac593a08ba
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-05-19 13:34:33 +0000

    Don't register imported models, only simple functions.

--- branches/shotserver-django/shotserver04/xmlrpc/views.py
+++ branches/shotserver-django/shotserver04/xmlrpc/views.py
@@ -11,7 +11,7 @@ def xmlrpc(request):
     response['Content-length'] = str(len(response.content))
     return response
 
-
+function_type = type(xmlrpc)
 dispatcher = Dispatcher()
 for app in settings.INSTALLED_APPS:
     try:
@@ -19,6 +19,6 @@ for app in settings.INSTALLED_APPS:
     except ImportError:
         continue
     for name, item in module.__dict__.items():
-        if callable(item):
+        if isinstance(item, function_type):
             function_name = '%s.%s' % (app.split('.')[-1], name)
             dispatcher.register_function(item, function_name)
