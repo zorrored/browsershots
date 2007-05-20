e24b2fe8b01865d3f33ecb0d6d3fc74cd2c2aa12
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-05-20 15:55:46 +0000

    Update last_poll timestamp.

--- branches/shotserver-django/shotserver04/requests/xmlrpc.py
+++ branches/shotserver-django/shotserver04/requests/xmlrpc.py
@@ -1,6 +1,7 @@
 from shotserver04.auth import util
 from shotserver04.factories.models import Factory
 from shotserver04.requests.models import Request
+from datetime import datetime
 
 
 def poll(request, factory_name, crypted_password):
@@ -14,11 +15,18 @@ def poll(request, factory_name, crypted_password):
     If an error occurs, the 'status' field in the result array will
     contain a short error message.
     """
+    # Verify authentication
     factory = Factory.objects.get(name=factory_name)
     ip = request.META['REMOTE_ADDR']
     status = util.verify(factory, ip, crypted_password)
     if status != 'OK':
         return {'status': status}
+
+    # Update last_poll timestamp
+    factory.last_poll = datetime.now()
+    factory.save()
+
+    # Find matching request
     matching_requests = Request.objects.filter(factory.features_q())
     if len(matching_requests) == 0:
         return {'status': 'No matching screenshot requests'}
