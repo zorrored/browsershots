9d7041dec62658cfe9c86758f8a32f54d489f771
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-05-31 10:30:41 +0000

    Removed unused imports.

--- branches/shotserver-django/shotserver04/factories/urls.py
+++ branches/shotserver-django/shotserver04/factories/urls.py
@@ -1,7 +1,4 @@
 from django.conf.urls.defaults import *
-from django.views.generic import list_detail
-from shotserver04.factories.models import Factory
-from shotserver04.browsers.models import Browser
 
 urlpatterns = patterns('shotserver04.factories.views',
     (r'^$', 'factory_list'),
