5497a63ee01edf19b03b946231619787c53892ef
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-06-05 20:00:26 +0000

    Storing browser and redirect timestamp in request table.

--- branches/shotserver-django/shotserver04/redirect/views.py
+++ branches/shotserver-django/shotserver04/redirect/views.py
@@ -3,13 +3,30 @@ from django.shortcuts import get_object_or_404, render_to_response
 from shotserver04.nonces import xmlrpc as nonces
 from shotserver04.factories.models import Factory
 from shotserver04.requests.models import Request
+from shotserver04.browsers.models import Browser
+from shotserver04.xmlrpc import ErrorMessage
+from datetime import datetime
 
 
-def redirect(request, factory_name, encrypted_password, request_id):
-    factory = get_object_or_404(Factory, name=factory_name)
-    status = nonces.verify(request, factory, encrypted_password)
-    if status != 'OK':
-        render_to_response('redirect/error.html', locals())
-    request = get_object_or_404(Request, id=request_id)
-    website = request.request_group.website
-    return HttpResponseRedirect(website.url)
+def redirect(http_request, factory_name, encrypted_password, request_id):
+    try:
+        factory = get_object_or_404(Factory, name=factory_name)
+        status = nonces.verify(http_request, factory, encrypted_password)
+        if status != 'OK':
+            raise ErrorMessage(status)
+        request = get_object_or_404(Request, id=request_id)
+        request.check_factory_lock(factory)
+        user_agent = http_request.META['HTTP_USER_AGENT']
+        try:
+            browser = Browser.objects.get(
+                factory=factory, user_agent=user_agent)
+        except Browser.DoesNotExist:
+            raise ErrorMessage("Unknown user agent: %s." % user_agent)
+        request.browser = browser
+        request.redirected = datetime.now()
+        request.save()
+        website = request.request_group.website
+        return HttpResponseRedirect(website.url)
+    except ErrorMessage, error:
+        return render_to_response('redirect/error.html',
+                                  {'message': error.message})
