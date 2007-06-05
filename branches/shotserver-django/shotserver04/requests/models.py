443b97ead259806f831597e5dd406cd92f2cd25b
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-06-05 21:36:06 +0000

    Added upload timestamp.

--- branches/shotserver-django/shotserver04/requests/models.py
+++ branches/shotserver-django/shotserver04/requests/models.py
@@ -5,6 +5,7 @@ from shotserver04.websites.models import Website
 from shotserver04.platforms.models import Platform
 from shotserver04.factories.models import Factory
 from shotserver04.browsers.models import BrowserGroup, Browser
+from shotserver04.xmlrpc import ErrorMessage
 
 
 class RequestGroup(models.Model):
@@ -71,6 +72,8 @@ class Request(models.Model):
         verbose_name=_('browser'), blank=True, null=True)
     redirected = models.DateTimeField(
         _('redirected'), blank=True, null=True)
+    uploaded =  models.DateTimeField(
+        _('uploaded'), blank=True, null=True)
 
     class Admin:
         fields = (
