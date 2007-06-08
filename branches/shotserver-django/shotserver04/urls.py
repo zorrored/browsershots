0d37358adadd594e6549c9333dd5ce34dbe5d260
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-06-08 19:31:36 +0000

    Added source file headers with license information, docstrings, and Rev Date Author tags.

--- branches/shotserver-django/shotserver04/urls.py
+++ branches/shotserver-django/shotserver04/urls.py
@@ -1,3 +1,39 @@
+# browsershots.org - Test your web design in different browsers
+# Copyright (C) 2007 Johann C. Rocholl <johann@browsershots.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+# MA 02111-1307, USA.
+
+"""
+Global URL configuration.
+
+This dispatches to the configuration for each installed app according
+to the first part of the request URL.
+
+URLs of the form http://browsershots.org/http://www.example.com/
+will be handled by the websites app.
+
+If settings.DEBUG is enabled (typically when the development server is
+running), URLs starting with /static/ or /png/ will be handled by
+Django, otherwise you should include them in the Apache configuration.
+"""
+
+__revision__ = "$Rev$"
+__date__ = "$Date$"
+__author__ = "$Author$"
+
 from django.conf.urls.defaults import patterns, include
 from shotserver04 import settings
 
