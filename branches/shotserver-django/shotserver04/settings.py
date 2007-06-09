b7f16683afd478fdd2b909514889276572d4a416
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-06-09 17:23:21 +0000

    Don't append slash to URLs.

--- branches/shotserver-django/shotserver04/settings.py
+++ branches/shotserver-django/shotserver04/settings.py
@@ -46,6 +46,10 @@ DATABASE_PORT = ''             # Set to empty string for default.
 # system time zone.
 TIME_ZONE = 'America/Chicago'
 
+# Don't append slash to URLs because of
+# http://browsershots.org/http://www.example.com/no-slash
+APPEND_SLASH = False
+
 # Language code for this installation. All choices can be found here:
 # http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
 # http://blogs.law.harvard.edu/tech/stories/storyReader$15
