27a8d29490443785de0298133e83fd65e2d1dd1c
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-06-16 19:47:11 +0000

    Moved initial data to custom SQL folders for models.

--- branches/shotserver-django/sql/platforms_architecture.sql
+++
@@ -1,6 +0,0 @@
-COPY platforms_architecture (id, name) FROM stdin;
-1	i686
-2	PPC
-\.
-
-SELECT 'platforms_architecture' AS table_name,  setval('platforms_architecture_id_seq', (SELECT max(id) FROM platforms_architecture)) as pkey_max;
