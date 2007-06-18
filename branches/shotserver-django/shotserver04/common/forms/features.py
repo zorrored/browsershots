a9f56c56919ee29f7aa6ae6954ed4f29f9d2148c
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-06-18 15:51:39 +0000

    Added header comments.

--- branches/shotserver-django/shotserver04/common/forms/features.py
+++ branches/shotserver-django/shotserver04/common/forms/features.py
@@ -1,3 +1,29 @@
+# browsershots.org - Test your web design in different browsers
+# Copyright (C) 2007 Johann C. Rocholl <johann@browsershots.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+# MA 02111-1307, USA.
+
+"""
+Input form with select fields for Javascript, Java, Flash.
+"""
+
+__revision__ = "$Rev$"
+__date__ = "$Date$"
+__author__ = "$Author$"
+
 from django import newforms as forms
 from shotserver04.browsers.models import Browser
 from shotserver04.features.models import Javascript, Java, Flash
