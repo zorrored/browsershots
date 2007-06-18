568649cf317306439295d2a8b39af7b40b580b12
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-06-18 15:47:34 +0000

    Improved docstrings.

--- branches/shotserver-django/shotserver04/common/middleware.py
+++ branches/shotserver-django/shotserver04/common/middleware.py
@@ -1,17 +1,48 @@
+# browsershots.org - Test your web design in different browsers
+# Copyright (C) 2007 Johann C. Rocholl <johann@browsershots.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+# MA 02111-1307, USA.
+
+"""
+Custom URL rewriting, more granular than Django's APPEND_SLASH.
+"""
+
+__revision__ = "$Rev$"
+__date__ = "$Date$"
+__author__ = "$Author$"
+
 from django.conf import settings
 from django import http
 
 
 class RedirectMiddleware(object):
     """
-    Append missing slashes, but if path starts with the name of an
-    installed app. Special URLs will be left untouched.
+    Append missing slashes, but only if path starts with the name of
+    an installed app. Special URLs like this will be left untouched::
+
+      http://browsershots.org/http://www.example.com/no-slash
 
     Also redirect to XML-RPC documentation if subdomain is 'api' or
     'xmlrpc' and path is '/'.
     """
 
     def process_request(self, request):
+        """
+        Process an incoming HTTP GET or HEAD request.
+        """
         # Don't rewrite POST requests
         if not request.method in ('GET', 'HEAD'):
             return
