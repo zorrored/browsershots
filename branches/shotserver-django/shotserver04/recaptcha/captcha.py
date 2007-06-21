e50db5612662edd4b06b9d7910ffd1f0ccd5c7f7
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-06-21 06:44:01 +0000

    Accept reCAPTCHA options.

--- branches/shotserver-django/shotserver04/recaptcha/captcha.py
+++ branches/shotserver-django/shotserver04/recaptcha/captcha.py
@@ -14,36 +14,47 @@ class RecaptchaResponse(object):
         self.is_valid = is_valid
         self.error_code = error_code
 
-def displayhtml (public_key,
-                 use_ssl = False,
-                 error = None):
-    """Gets the HTML to display for reCAPTCHA
+def displayhtml (public_key, use_ssl=False, error=None,
+                 options={}):
+    """
+    Get the HTML to display for a reCAPTCHA challenge.
 
     public_key -- The public api key
     use_ssl -- Should the request be sent over ssl?
-    error -- An error message to display (from RecaptchaResponse.error_code)"""
-
+    error -- An error message to display (from RecaptchaResponse.error_code)
+    options -- A dict, e.g. {'theme': 'white', 'tabindex': 2}
+    """
     error_param = ''
     if error:
 	error_param = '&error=%s' % error
-
     if use_ssl:
-        server = 'https://' + API_SSL_SERVER
+        protocol = 'https'
+        server = API_SSL_SERVER
     else:
-        server = 'http://' + API_SERVER
-
-    return """<script type="text/javascript" src="%(ApiServer)s/challenge?k=%(PublicKey)s%(ErrorParam)s"></script>
+        protocol = 'http'
+        server = API_SERVER
+    options_lines = []
+    for key in options:
+        options_lines.append('%s: %s' % (key, repr(options[key])))
+    options_script = ''
+    if options_lines:
+        options_script = """
+<script type="text/javascript">
+var RecaptchaOptions = {
+  %s
+}
+</script>
+""".strip() % ',\n  '.join(options_lines)
+    return """
+%(options_script)s
+<script type="text/javascript" src="%(protocol)s://%(server)s/challenge?k=%(public_key)s%(error_param)s"></script>
 
 <noscript>
-  <iframe src="%(ApiServer)s/noscript?k=%(PublicKey)s%(ErrorParam)s" height="300" width="500" frameborder="0"></iframe><br />
+  <iframe src="%(protocol)s://%(server)s/noscript?k=%(public_key)s%(error_param)s" height="300" width="500" frameborder="0"></iframe><br />
   <textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
   <input type='hidden' name='recaptcha_response_field' value='manual_challenge' />
 </noscript>
-""" % {
-        'ApiServer' : server,
-        'PublicKey' : public_key,
-        'ErrorParam' : error_param,
-        }
+""".strip() % locals()
 
 
 def submit (recaptcha_challenge_field,
