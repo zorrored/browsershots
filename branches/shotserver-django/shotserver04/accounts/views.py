abc70b8069727a0cf21ffbcc980fb56cc0ba810d
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-06-30 21:51:52 +0000

    Show user's screenshot factories on his profile page.

--- branches/shotserver-django/shotserver04/accounts/views.py
+++ branches/shotserver-django/shotserver04/accounts/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
+from shotserver04.factories.models import Factory
 
 
 @login_required
 def profile(http_request):
+    factory_table_header = Factory.table_header()
+    factory_list = Factory.objects.filter(admin=http_request.user)
     return render_to_response('accounts/profile.html', locals())
