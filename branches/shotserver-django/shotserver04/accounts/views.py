5b5f09beb9d561bbe57f290ecc9f646c1be8c182
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-06-30 19:19:17 +0000

    Require login for profile page.

--- branches/shotserver-django/shotserver04/accounts/views.py
+++ branches/shotserver-django/shotserver04/accounts/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 
 
+@login_required
 def profile(http_request):
     return render_to_response('accounts/profile.html', locals())
