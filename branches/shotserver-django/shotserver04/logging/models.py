cd61184ed2a052b6ed2e0577546dccd99f4418ef
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-07-01 00:01:47 +0000

    Simple error logging in the database.

---
+++ branches/shotserver-django/shotserver04/logging/models.py
@@ -0,0 +1,52 @@
+# browsershots.org - Test your web design in different browsers
+# Copyright (C) 2007 Johann C. Rocholl <johann@browsershots.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+# MA 02111-1307, USA.
+
+"""
+Error log models.
+"""
+
+__revision__ = "$Rev$"
+__date__ = "$Date$"
+__author__ = "$Author$"
+
+from django.db import models
+from shotserver04.factories.models import Factory
+
+
+class FactoryError(models.Model):
+    """
+    Database log for errors in the screenshot factory interface.
+    """
+    factory = models.ForeignKey(Factory,
+        verbose_name=_("factory"))
+    code = models.IntegerField(
+        _("error code"))
+    message = models.CharField(
+        _("error message"), maxlength=600)
+    occurred = models.DateTimeField(
+        _("occurred"), auto_now_add=True)
+
+    class Admin:
+        pass
+
+    class Meta:
+        verbose_name = _("factory error message")
+        verbose_name = _("factory error messages")
+
+    def __str__(self):
+        return self.message
