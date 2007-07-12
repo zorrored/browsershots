951aa6d312f9a9d667287280de9697c4890f8ad1
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-07-12 17:55:49 +0000

    Added MSIE engine.

--- branches/shotserver-django/shotserver04/browsers/sql/engine.sql
+++ branches/shotserver-django/shotserver04/browsers/sql/engine.sql
@@ -2,4 +2,5 @@ INSERT INTO "browsers_engine" ("id","name","maker") VALUES (1,'Gecko','Mozilla')
 INSERT INTO "browsers_engine" ("id","name","maker") VALUES (2,'KHTML','KDE');
 INSERT INTO "browsers_engine" ("id","name","maker") VALUES (3,'Opera','Opera');
 INSERT INTO "browsers_engine" ("id","name","maker") VALUES (4,'AppleWebKit','Apple');
+INSERT INTO "browsers_engine" ("id","name","maker") VALUES (5,'MSIE','Microsoft');
 SELECT setval('browsers_engine_id_seq', (SELECT max("id") FROM "browsers_engine"));
