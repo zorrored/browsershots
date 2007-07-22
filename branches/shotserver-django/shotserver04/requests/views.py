2028bbf6362d3a04be21f3a436ec2e385fafdbfc
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-07-22 00:52:49 +0000

    Removed useless database queries.

--- branches/shotserver-django/shotserver04/requests/views.py
+++ branches/shotserver-django/shotserver04/requests/views.py
@@ -79,8 +79,6 @@ GROUP BY platform_id, browser_group_id, major, minor
 def details(http_request, group):
     request_group = get_object_or_404(RequestGroup, pk=group)
     website = request_group.website
-    request_list = request_group.request_set.all()
-    preload_foreign_keys(request_list, browser_group=True)
     return render_to_response('requests/details.html', locals())
 
 
