d7b2bd266aaf59dfd394c474f55a05facf24a4db
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-08-19 05:47:08 +0000

    Moving version 0.3 out of trunk for 0.4.

--- shotserver/scripts/shotserver03_db_vacuum.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# This script must be run as the root database owner, usually postgres.
-echo "VACUUM ANALYZE;" | psql shotserver03
-echo "VACUUM ANALYZE;" | psql shotserver03_trac010
--- shotserver/scripts/shotserver03_mailman_backup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-ARCHIVES=/var/lib/mailman/archives/private
-BACKUP=/backup/`hostname`$ARCHIVES.`date +%Y-%m-%d`.tar.bz2
-tar -cjPf $BACKUP $ARCHIVES
