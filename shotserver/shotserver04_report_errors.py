078178d5349fb145412845b60e2162ef58ec5c59
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-08-29 21:25:54 +0000

    Formatting, docstrings, comments, helper functions.

--- shotserver/shotserver04_report_errors.py
+++ shotserver/shotserver04_report_errors.py
@@ -4,10 +4,11 @@ import sys
 import re
 import socket
 
-MAX_EXAMPLES = 5
-ORPHANS = 3
-INDENT = ' ' * 3
+NUMERIC = '-n' in sys.argv # No DNS queries if called with -n argument.
+MAX_EXAMPLES = 5 # Can be overriden with the max_examples parameter.
+ORPHANS = 3 # Show more examples if only few are left.
 
+# Regular expression for combined log format.
 log_match = re.compile(r"""
 (?P<ip>\d+\.\d+\.\d+\.\d+)\s
 (\S+)\s
@@ -25,7 +26,20 @@ log_match = re.compile(r"""
 """, re.VERBOSE).match
 
 
+def plural(noun):
+    """
+    Pluralize an english noun.
+    """
+    if noun.endswith('s'):
+        return noun + 'es' # statuses
+    else:
+        return  noun + 's'
+
+
 def categorize(matches, key, preposition='from', max_examples=MAX_EXAMPLES):
+    """
+    Show the most common values for a selected key.
+    """
     categories = {}
     for match in matches:
         value = match.group(key)
@@ -33,26 +47,29 @@ def categorize(matches, key, preposition='from', max_examples=MAX_EXAMPLES):
     counts = [(count, category) for category, count in categories.iteritems()]
     counts.sort()
     counts.reverse()
-    if key == 'path':
-        print '=' * (61 + len(INDENT))
-    elif len(counts) > 1:
-        print INDENT, '-' * 60
+    if key != 'path' and len(counts) > 1:
+        print '-' * 40
     stop = min(len(counts), max_examples)
     if stop + ORPHANS >= len(counts):
         stop = len(counts)
     for index in range(stop):
         count, category = counts[index]
-        if key == 'ip':
+        if key == 'ip' and not NUMERIC:
             category += ' (%s)' % socket.getfqdn(category)
         if count == len(matches):
             count = 'all'
-        print INDENT, count, preposition, key, category
+            if key == 'path' and category.count('/') == 1:
+                break
+        print count, preposition, key, category
     if stop < len(counts):
         rest = sum([count[0] for count in counts[stop:]])
-        print INDENT, rest, preposition, len(counts) - stop, 'other', key + 's'
+        print rest, preposition, len(counts) - stop, 'other', plural(key)
 
 
 def error_details(matches):
+    """
+    Show details about this group of errors.
+    """
     categorize(matches, 'path', preposition='for', max_examples=10)
     categorize(matches, 'status', preposition='with')
     categorize(matches, 'method', preposition='with')
@@ -61,15 +78,25 @@ def error_details(matches):
     categorize(matches, 'useragent')
 
 
+def get_first_part(path):
+    """
+    Get the part before the first / or ? or #.
+    """
+    result = path.lstrip('/')
+    for separator in '/?#':
+        result = result.split(separator)[0]
+    return result
+
+
 # Collect all errors by first part of URL path.
 errors = {}
 for line in sys.stdin:
     match = log_match(line)
     if match is None:
-        continue
-    if match.group('status')[0] in '23':
-        continue
-    firstpart = match.group('path').lstrip('/').split('/')[0]
+        continue # Access log line could not be parsed.
+    if match.group('status')[0] in '123':
+        continue # Status codes 100, 200, 300 aren't error messages.
+    firstpart = get_first_part(match.group('path'))
     if firstpart not in errors:
         errors[firstpart] = []
     errors[firstpart].append(match)
@@ -83,6 +110,8 @@ counts.reverse()
 
 # Print errors, most common first, with supporting info
 for count, firstpart in counts:
+    print '=' * 60
     print count, 'errors for /%s' % firstpart
+    print '=' * 60
     error_details(errors[firstpart])
     print
