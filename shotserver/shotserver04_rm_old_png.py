4bdf2a09def9f38bdcb8757c74e5e98b4aee375b
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-10-06 03:49:48 +0000

    Delete oldest files until we have sufficient free disk space.

--- shotserver/shotserver04_rm_old_png.py
+++ shotserver/shotserver04_rm_old_png.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 # Delete old PNG files
-#
-# This script expects a list of candidates on stdin, like this:
-# /var/www/v04.browsershots.org/png/160/74/749a5c7602d6de93d9a5bfdc2e7db1a9.png
-# /var/www/v04.browsershots.org/png/160/74/742d8deb15f0cf2cd0a347f7dc3c58f7.png
-# /var/www/v04.browsershots.org/png/160/74/7492c8add13e09205a3c788d47f9e599.png
-#
-# You can use the following command to find good candidates:
-# find /var/www/v04.browsershots.org/png/160 -atime +10
 
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'shotserver04.settings'
@@ -15,52 +7,89 @@ import sys
 import glob
 import time
 from shotserver04.screenshots.models import Screenshot
+from shotserver04.settings import PNG_ROOT
 
-NOW = time.time()
-EXPIRE_DAYS = 6
-EXPIRE = NOW - EXPIRE_DAYS * 24 * 3600
-PNG_ROOT = '/var/www/v04.browsershots.org/png/'
-FOLDERS = glob.glob(PNG_ROOT + '*')
-
-
-def png_filename(folder, hashkey):
-    return os.path.join(folder, hashkey[:2], hashkey + '.png')
+FOLDERS = glob.glob(os.path.join(PNG_ROOT, '*'))
+CANDIDATE_FOLDER = os.path.join(PNG_ROOT, '160')
+MAX_CANDIDATES = 5000
+MIN_FREE_PERCENT = 10
 
 
-def _main():
+def timestamp(when=None):
+    return '%04d-%02d-%02d %02d:%02d:%02d' % time.localtime(when)[:6]
+
+
+def disk_free_percent():
     stat = os.statvfs(PNG_ROOT)
-    total_disk_space = stat.f_blocks * stat.f_frsize
-    free_disk_space = stat.f_bavail * stat.f_frsize
-    free_disk_percent = 100 * stat.f_bavail / stat.f_blocks
-    if free_disk_percent >= 10:
-        print "%d bytes free (%d%%)" % (free_disk_space, free_disk_percent)
-        return 0
-    # Delete old screenshots
-    screenshot_count = unlink_count = 0
-    for candidate in sys.stdin:
-        basename = os.path.basename(candidate.rstrip())
-        if len(basename) != 36:
-            print basename, len(basename)
-        hashkey = basename[:32]
-        filenames = [png_filename(folder, hashkey) for folder in FOLDERS]
-        filenames = filter(os.path.exists, filenames)
-        if not filenames:
+    # total_disk_space = stat.f_blocks * stat.f_frsize
+    # free_disk_space = stat.f_bavail * stat.f_frsize
+    return 100.0 * stat.f_bavail / stat.f_blocks
+
+
+def collect_candidates():
+    print timestamp(), 'collecting...'
+    hashkeys = []
+    for dirname in os.listdir(CANDIDATE_FOLDER):
+        dirname_full = os.path.join(CANDIDATE_FOLDER, dirname)
+        if not os.path.isdir(dirname_full):
             continue
-        atimes = [os.stat(filename).st_atime for filename in filenames]
-        if max(atimes) > EXPIRE:
-            continue
-        # print chr(13), hashkey, len(atimes), max(atimes),
-        # print 'delete', hashkey
-        Screenshot.objects.filter(hashkey=hashkey).delete()
-        screenshot_count += 1
-        for filename in filenames:
-            # print 'unlink', filename
-            os.unlink(filename)
-        unlink_count += len(filenames)
-    print "deleted %d screenshots (%d files)" % (
-        screenshot_count, unlink_count)
+        # print dirname_full
+        for pngname in os.listdir(dirname_full):
+            if len(pngname) != 36:
+                continue
+            pngname_full = os.path.join(dirname_full, pngname)
+            atime = os.stat(pngname_full).st_atime
+            hashkey = pngname[:32]
+            hashkeys.append((atime, hashkey))
+    print timestamp(), 'found %d screenshots, sorting...' % len(hashkeys)
+    hashkeys.sort()
+    hashkeys = hashkeys[:MAX_CANDIDATES]
+    return hashkeys
+
+
+def hashkey_filenames(hashkey):
+    return filter(os.path.exists,
+        [os.path.join(folder, hashkey[:2], hashkey + '.png')
+        for folder in FOLDERS])
+
+
+def update_candidates(hashkeys):
+    print timestamp(), 'updating...'
+    for index in xrange(len(hashkeys)):
+        atime, hashkey = hashkeys[index]
+        max_atime = max([os.stat(filename).st_atime
+            for filename in hashkey_filenames(hashkey)])
+        if max_atime > atime:
+            #print hashkey, timestamp(atime), timestamp(max_atime)
+            hashkeys[index] = (max_atime, hashkey)
+    print timestamp(), 'sorting again...'
+    hashkeys.sort()
 
 
 if __name__ == '__main__':
-    # print EXPIRE
-    _main()
+    free = disk_free_percent()
+    if free > MIN_FREE_PERCENT:
+        print '%.3f%% free, nothing to do' % free
+        sys.exit(0)
+
+    hashkeys = collect_candidates()
+    last_atime = hashkeys[-1][0]
+    update_candidates(hashkeys)
+
+    screenshot_count = 0
+    png_count = 0
+    for atime, hashkey in hashkeys:
+        if atime > last_atime:
+            break
+        free = disk_free_percent()
+        if free > MIN_FREE_PERCENT:
+            break
+        print '%.3f' % free, hashkey, timestamp(atime)
+        Screenshot.objects.filter(hashkey=hashkey).delete()
+        screenshot_count += 1
+        for filename in hashkey_filenames(hashkey):
+            # print 'unlink', filename
+            os.unlink(filename)
+            png_count += 1
+    print timestamp(), "deleted %d screenshots (%d files)" % (
+        screenshot_count, png_count)
