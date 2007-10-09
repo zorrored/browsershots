c35b8fd39554eec357f96e4c6f067210f61b4d3e
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-10-09 20:51:37 +0000

    Choose usage interval in the URL.

--- shotserver/shotserver04/status/views.py
+++ shotserver/shotserver04/status/views.py
@@ -8,8 +8,6 @@ from django.contrib.auth.decorators import login_required
 from shotserver04 import settings
 from shotserver04.websites.models import Website, Domain
 
-USAGE_INTERVAL_DAYS = 7
-
 
 @login_required
 def overview(http_request):
@@ -29,17 +27,17 @@ def overview(http_request):
 
 
 @login_required
-def usage(http_request):
+def usage(http_request, usage_interval='7d'):
     cursor = connection.cursor()
     # Most frequently requested websites
     cursor.execute("""\
 SELECT website_id, COUNT(*) AS groups
 FROM requests_requestgroup
-WHERE requests_requestgroup.submitted > NOW() - '%dd'::interval
+WHERE requests_requestgroup.submitted > NOW() - %s::interval
 GROUP BY website_id
 ORDER BY groups DESC
 LIMIT 10
-""" % USAGE_INTERVAL_DAYS)
+""", (usage_interval, ))
     rows = cursor.fetchall()
     website_dict = Website.objects.in_bulk([row[0] for row in rows])
     website_list = [website_dict[row[0]] for row in rows]
@@ -50,11 +48,11 @@ LIMIT 10
 SELECT domain_id, COUNT(*) AS groups
 FROM requests_requestgroup
 JOIN websites_website ON (websites_website.id = website_id)
-WHERE requests_requestgroup.submitted > NOW() - '%dd'::interval
+WHERE requests_requestgroup.submitted > NOW() - %s::interval
 GROUP BY domain_id
 ORDER BY groups DESC
 LIMIT 10
-""" % USAGE_INTERVAL_DAYS)
+""", (usage_interval, ))
     rows = cursor.fetchall()
     domain_dict = Domain.objects.in_bulk([row[0] for row in rows])
     domain_list = [domain_dict[row[0]] for row in rows]
@@ -64,13 +62,14 @@ LIMIT 10
     cursor.execute("""\
 SELECT ip, COUNT(*) AS groups
 FROM requests_requestgroup
-WHERE requests_requestgroup.submitted > NOW() - '%dd'::interval
+WHERE requests_requestgroup.submitted > NOW() - %s::interval
 GROUP BY ip
 ORDER BY groups DESC
 LIMIT 10
-""" % USAGE_INTERVAL_DAYS)
+""", (usage_interval, ))
     rows = cursor.fetchall()
     ip_list = [(row[0], socket.getfqdn(row[0]), row[1]) for row in rows]
-    usage_interval = '%d days' % USAGE_INTERVAL_DAYS
+    usage_interval = usage_interval.replace('d', ' days')
+    usage_interval = usage_interval.replace('h', ' hours')
     return render_to_response('status/usage.html', locals(),
         context_instance=RequestContext(http_request))
