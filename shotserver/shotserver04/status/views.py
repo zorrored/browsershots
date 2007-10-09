ab332468e795fb41db317ab42a62df298c280035
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-10-09 20:38:58 +0000

    Change usage status interval to 7 days with new constant USAGE_INTERVAL_DAYS.

--- shotserver/shotserver04/status/views.py
+++ shotserver/shotserver04/status/views.py
@@ -8,6 +8,8 @@ from django.contrib.auth.decorators import login_required
 from shotserver04 import settings
 from shotserver04.websites.models import Website, Domain
 
+USAGE_INTERVAL_DAYS = 7
+
 
 @login_required
 def overview(http_request):
@@ -33,11 +35,11 @@ def usage(http_request):
     cursor.execute("""\
 SELECT website_id, COUNT(*) AS groups
 FROM requests_requestgroup
-WHERE requests_requestgroup.submitted > NOW() - '24h'::interval
+WHERE requests_requestgroup.submitted > NOW() - '%dd'::interval
 GROUP BY website_id
 ORDER BY groups DESC
 LIMIT 10
-""")
+""" % USAGE_INTERVAL_DAYS)
     rows = cursor.fetchall()
     website_dict = Website.objects.in_bulk([row[0] for row in rows])
     website_list = [website_dict[row[0]] for row in rows]
@@ -48,11 +50,11 @@ LIMIT 10
 SELECT domain_id, COUNT(*) AS groups
 FROM requests_requestgroup
 JOIN websites_website ON (websites_website.id = website_id)
-WHERE requests_requestgroup.submitted > NOW() - '24h'::interval
+WHERE requests_requestgroup.submitted > NOW() - '%dd'::interval
 GROUP BY domain_id
 ORDER BY groups DESC
 LIMIT 10
-""")
+""" % USAGE_INTERVAL_DAYS)
     rows = cursor.fetchall()
     domain_dict = Domain.objects.in_bulk([row[0] for row in rows])
     domain_list = [domain_dict[row[0]] for row in rows]
@@ -62,12 +64,13 @@ LIMIT 10
     cursor.execute("""\
 SELECT ip, COUNT(*) AS groups
 FROM requests_requestgroup
-WHERE requests_requestgroup.submitted > NOW() - '24h'::interval
+WHERE requests_requestgroup.submitted > NOW() - '%dd'::interval
 GROUP BY ip
 ORDER BY groups DESC
 LIMIT 10
-""")
+""" % USAGE_INTERVAL_DAYS)
     rows = cursor.fetchall()
     ip_list = [(row[0], socket.getfqdn(row[0]), row[1]) for row in rows]
+    usage_interval = '%d days' % USAGE_INTERVAL_DAYS
     return render_to_response('status/usage.html', locals(),
         context_instance=RequestContext(http_request))
