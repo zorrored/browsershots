71c4a5b3406840d546827f3710defec0c3204401
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-10-22 18:18:57 +0000

    Import ugettext_lazy as _ because it's no longer automatically visible through Django.

--- trunk/shotserver/shotserver04/messages/models.py
+++ trunk/shotserver/shotserver04/messages/models.py
@@ -23,6 +23,7 @@ __date__ = "$Date$"
 __author__ = "$Author$"
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 from shotserver04.factories.models import Factory
 
 
