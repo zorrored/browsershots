d81a3fa5cea52b6f427b624f889f371f636fcb5a
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-10-25 11:58:53 +0000

    Show summary when finished, don't show individual screenshots. Increased MAX_CANDIDATES from 5000 to 20000.

--- trunk/shotserver/shotserver04_rm_old_png.py
+++ trunk/shotserver/shotserver04_rm_old_png.py
@@ -33,8 +33,8 @@ from shotserver04.settings import PNG_ROOT
 
 FOLDERS = glob.glob(os.path.join(PNG_ROOT, '*'))
 CANDIDATE_FOLDER = os.path.join(PNG_ROOT, '160')
-MAX_CANDIDATES = 5000
-MIN_FREE_PERCENT = 15
+MAX_CANDIDATES = 20000
+MIN_FREE_PERCENT = 10
 if len(sys.argv) == 2:
     MIN_FREE_PERCENT = float(sys.argv[-1])
 
@@ -43,15 +43,18 @@ def timestamp(when=None):
     return '%04d-%02d-%02d %02d:%02d:%02d' % time.localtime(when)[:6]
 
 
+def disk_free_bytes():
+    stat = os.statvfs(PNG_ROOT)
+    return stat.f_bavail * stat.f_frsize
+
+
 def disk_free_percent():
     stat = os.statvfs(PNG_ROOT)
-    # total_disk_space = stat.f_blocks * stat.f_frsize
-    # free_disk_space = stat.f_bavail * stat.f_frsize
     return 100.0 * stat.f_bavail / stat.f_blocks
 
 
 def collect_candidates():
-    print timestamp(), 'collecting...'
+    print timestamp(), "collecting..."
     hashkeys = []
     for dirname in os.listdir(CANDIDATE_FOLDER):
         dirname_full = os.path.join(CANDIDATE_FOLDER, dirname)
@@ -65,7 +68,7 @@ def collect_candidates():
             atime = os.stat(pngname_full).st_atime
             hashkey = pngname[:32]
             hashkeys.append((atime, hashkey))
-    print timestamp(), 'found %d screenshots, sorting...' % len(hashkeys)
+    print timestamp(), "found %d screenshots, sorting..." % len(hashkeys)
     hashkeys.sort()
     hashkeys = hashkeys[:MAX_CANDIDATES]
     return hashkeys
@@ -78,7 +81,7 @@ def hashkey_filenames(hashkey):
 
 
 def update_candidates(hashkeys):
-    print timestamp(), 'updating...'
+    print timestamp(), "updating..."
     for index in xrange(len(hashkeys)):
         atime, hashkey = hashkeys[index]
         max_atime = max([os.stat(filename).st_atime
@@ -86,29 +89,22 @@ def update_candidates(hashkeys):
         if max_atime > atime:
             #print hashkey, timestamp(atime), timestamp(max_atime)
             hashkeys[index] = (max_atime, hashkey)
-    print timestamp(), 'sorting again...'
+    print timestamp(), "sorting again..."
     hashkeys.sort()
 
 
-if __name__ == '__main__':
-    free = disk_free_percent()
-    if free > MIN_FREE_PERCENT:
-        print '%.3f%% free, nothing to do' % free
-        sys.exit(0)
-
-    hashkeys = collect_candidates()
-    last_atime = hashkeys[-1][0]
-    update_candidates(hashkeys)
-
+def delete_some_screenshots(hashkeys, max_atime):
+    print timestamp(), "deleting screenshots..."
     screenshot_count = 0
     png_count = 0
     for atime, hashkey in hashkeys:
-        if atime > last_atime:
+        last = atime
+        if atime > max_atime:
             break
         free = disk_free_percent()
         if free > MIN_FREE_PERCENT:
             break
-        print '%.3f' % free, hashkey, timestamp(atime)
+        # print '%.3f' % free, hashkey, timestamp(atime)
         Screenshot.objects.filter(hashkey=hashkey).delete()
         screenshot_count += 1
         for filename in hashkey_filenames(hashkey):
@@ -117,3 +113,19 @@ if __name__ == '__main__':
             png_count += 1
     print timestamp(), "deleted %d screenshots (%d files)" % (
         screenshot_count, png_count)
+    print "with access times from %s to %s" % (
+        timestamp(hashkeys[0][0]), timestamp(last))
+    print "free disk space now %.2f GiB (%.2f%%)" % (
+        disk_free_bytes() / 1024.0 / 1024.0 / 1024.0,
+        disk_free_percent())
+
+
+if __name__ == '__main__':
+    free = disk_free_percent()
+    if free > MIN_FREE_PERCENT:
+        print "%.3f%% free, nothing to do" % free
+        sys.exit(0)
+    hashkeys = collect_candidates()
+    max_atime = hashkeys[-1][0]
+    update_candidates(hashkeys)
+    delete_some_screenshots(hashkeys, max_atime)
