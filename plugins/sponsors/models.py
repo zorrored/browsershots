dd02642073b06720397bd9534cd4a95feb7261f7
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-11-23 19:06:14 +0000

    Added update_fields method.

--- plugins/sponsors/models.py
+++ plugins/sponsors/models.py
@@ -25,6 +25,7 @@ __author__ = "$Author$"
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.utils.safestring import mark_safe
+from shotserver04.common import granular_update
 
 
 class Sponsor(models.Model):
@@ -52,6 +53,8 @@ class Sponsor(models.Model):
         verbose_name = _('sponsor')
         verbose_name_plural = _('sponsors')
         ordering = ('-premium', 'name', )
+
+    update_fields = granular_update.update_fields
 
     def __unicode__(self):
         return self.name
