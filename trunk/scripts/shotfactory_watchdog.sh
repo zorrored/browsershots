ed991b16f49b9484f2b7c655fe72387dee37e6c4
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-12-25 23:09:24 +0000

    Simply pass through all command line arguments.

--- trunk/scripts/shotfactory_watchdog.sh
+++ trunk/scripts/shotfactory_watchdog.sh
@@ -2,17 +2,12 @@
 # Restart screenshot factories when they die, or after reboot.
 #
 # Add me to /etc/crontab like this (make sure I'm on the PATH):
-# * *   * * *   shotfactory1   shotfactory_watchdog.sh <password>
-# * *   * * *   shotfactory2   shotfactory_watchdog.sh <password>
-# * *   * * *   shotfactory3   shotfactory_watchdog.sh <password>
+# * *   * * *   shotfactory1   shotfactory_watchdog.sh -d :1 -l 1
+# * *   * * *   shotfactory2   shotfactory_watchdog.sh -d :2 -l 2
+# * *   * * *   shotfactory3   shotfactory_watchdog.sh -d :3 -l 3
 #
-# The load limit for each factory will be set to the factory index
-# (the number at the end of the username). So shotfactory1 will take a
-# break if the load average is above 1, shotfactory2 will continue to
-# work until the load average is above 2, shotfactory3 until load 3.
-PASSWORD=$1
+# All parameters will be passed to the shotfactory.py script.
 USER=`whoami`
-INDEX=`whoami | sed s/shotfactory//`
 
 # Check if screen is already running.
 ps -u $USER | grep screen > /dev/null && exit 0
@@ -33,4 +28,4 @@ cd /home/$USER/checkout/shotfactory
 
 # Start screen (detached, with logging).
 # Set the display (-d) and load limit (-l) to the factory index.
-screen -d -m -L python shotfactory.py -p $PASSWORD -d :$INDEX -l $INDEX
+screen -d -m -L python shotfactory.py "$@"
