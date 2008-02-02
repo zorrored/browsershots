deeae769dc3a31f156792bcc0eddd05c87da32e8
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-02-02 18:27:11 +0000

    Replaced FloatField with DecimalField for financial amounts.

--- trunk/plugins/priority/models.py
+++ trunk/plugins/priority/models.py
@@ -34,7 +34,7 @@ class UserPriority(models.Model):
     expire = models.DateTimeField()
     txn_id = models.CharField(max_length=40)
     currency = models.CharField(max_length=3)
-    payment = models.FloatField()
+    payment = models.DecimalField(max_digits=7, decimal_places=2)
 
     class Admin:
         list_display = ('user', 'priority', 'activated', 'expire',
