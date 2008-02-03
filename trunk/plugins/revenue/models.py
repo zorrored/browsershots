e56468181c857cd9aedb5b59179dad67d4cf8c6f
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-02-03 17:14:38 +0000

    Added model UserRevenue and function latest_balance().

--- trunk/plugins/revenue/models.py
+++ trunk/plugins/revenue/models.py
@@ -21,10 +21,36 @@ __revision__ = "$Rev$"
 __date__ = "$Date$"
 __author__ = "$Author$"
 
+from decimal import Decimal
+from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User
 from shotserver04.factories.models import Factory
 from shotserver04.common import granular_update
+
+
+class UserRevenue(models.Model):
+    user = models.ForeignKey(User, raw_id_admin=True)
+    year = models.IntegerField()
+    month = models.IntegerField()
+    screenshots = models.IntegerField()
+    percent = models.FloatField()
+    euros = models.DecimalField(max_digits=7, decimal_places=2)
+    balance = models.DecimalField(max_digits=7, decimal_places=2)
+    date = models.DateTimeField()
+
+    class Admin:
+        list_display = ('user', 'year', 'month', 'screenshots', 'percent',
+                        'euros', 'balance', 'date')
+
+    class Meta:
+        unique_together = ('user', 'year', 'month')
+
+    def __unicode__(self):
+        return u'revenue share for %d screenshots (%.3f%%) in %04d-%02d' % (
+            self.screenshots, self.percent, self.year, self.month)
+
+    update_fields = granular_update.update_fields
 
 
 class UserPayment(models.Model):
@@ -39,9 +65,9 @@ class UserPayment(models.Model):
         list_display = ('user', 'currency', 'amount', 'euros', 'date')
 
     def __unicode__(self):
-        return u'%s %.2f paid to %s on %s' % (
-            self.currency, self.amount, self.user,
-            self.paid.strftime('%Y-%m-%d'))
+        return u'payment of %s %s' % (self.currency, abs(self.amount))
+
+    update_fields = granular_update.update_fields
 
 
 class NonProfit(models.Model):
@@ -56,6 +82,8 @@ class NonProfit(models.Model):
 
     def get_absolute_url(self):
         return self.url
+
+    update_fields = granular_update.update_fields
 
 
 class UserDonation(models.Model):
@@ -72,6 +100,33 @@ class UserDonation(models.Model):
                         'currency', 'amount', 'euros', 'date')
 
     def __unicode__(self):
-        return u'%s %.2f donated to %s by %s on %s' % (
-            self.currency, self.amount, self.non_profit, self.user,
-            self.donated.strftime('%Y-%m-%d'))
+        return u'donation of %s %.2f to %s' % (
+            self.currency, abs(self.amount), self.non_profit)
+
+
+    update_fields = granular_update.update_fields
+
+
+def latest_balance(user, before=None):
+    """
+    Get the latest balance for a user, before a given date (default now).
+    """
+    if before is None:
+        before = datetime.now()
+    candidates = []
+    revenues = UserRevenue.objects.filter(
+        user=user, date__lt=before).order_by('-date')[:1]
+    payments = UserPayment.objects.filter(
+        user=user, date__lt=before).order_by('-date')[:1]
+    donations = UserDonation.objects.filter(
+        user=user, date__lt=before).order_by('-date')[:1]
+    if len(revenues):
+        candidates.append((revenues[0].date, revenues[0].balance))
+    if len(payments):
+        candidates.append((payments[0].date, payments[0].balance))
+    if len(donations):
+        candidates.append((donations[0].date, donations[0].balance))
+    if len(candidates):
+        candidates.sort()
+        return candidates[-1][1]
+    return Decimal('0.00')
