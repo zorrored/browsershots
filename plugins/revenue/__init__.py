bf7a280b0ac572cf835773111b1b212cf07be47a
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-02-03 17:13:41 +0000

    Moved function month_revenue() here from accounts/views.py.

--- plugins/revenue/__init__.py
+++ plugins/revenue/__init__.py
@@ -15,8 +15,30 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 """
+Utility functions for revenue sharing.
 """
 
 __revision__ = "$Rev$"
 __date__ = "$Date$"
 __author__ = "$Author$"
+
+from datetime import datetime
+from shotserver04.priority.models import UserPriority
+
+
+def month_revenue(year, month):
+    """
+    Get the total monthly shared revenue, in Euros.
+    """
+    if month == 12:
+        next_year = year + 1
+        next_month = 1
+    else:
+        next_year = year
+        next_month = month + 1
+    priorities = UserPriority.objects.filter(
+        activated__gte=datetime(year, month, 1),
+        activated__lt=datetime(next_year, next_month, 1))
+    euros = sum([p.payment for p in priorities.filter(currency='EUR')])
+    dollars = sum([p.payment for p in priorities.filter(currency='USD')])
+    return (float(euros) + (float(dollars) / 1.5)) / 2
