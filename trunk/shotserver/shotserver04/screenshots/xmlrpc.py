93afd2dae5e96d6946c3b3e2ad71cc0b85716945
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-02-22 14:59:04 +0000

    Removed unused preview sizes to reduce server load.

--- trunk/shotserver/shotserver04/screenshots/xmlrpc.py
+++ trunk/shotserver/shotserver04/screenshots/xmlrpc.py
@@ -36,7 +36,7 @@ from shotserver04.requests.models import Request
 from shotserver04.screenshots.models import Screenshot
 from shotserver04.screenshots import storage
 
-PREVIEW_SIZES = [512, 240, 160, 32]
+PREVIEW_SIZES = [512, 160]
 # PREVIEW_SIZES = [512, 240, 160, 116, 92, 77, 57, 44, 32]
 # PREVIEW_SIZES = [640, 316, 208, 154, 100, 64, 46]
 
