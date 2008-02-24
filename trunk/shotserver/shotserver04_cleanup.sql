6baa18f2ed5225bded5d06f7b605ad3ad9100e99
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-02-24 11:37:09 +0000

    Delete old browsers.

--- trunk/shotserver/shotserver04_cleanup.sql
+++ trunk/shotserver/shotserver04_cleanup.sql
@@ -1,21 +1,21 @@
--- Delete expired nonces.
+\echo 'Deleting expired nonces...'
 DELETE FROM nonces_nonce
 WHERE created < NOW() - '7d'::interval;
 
--- Delete old requests that don't have screenshots.
+\echo 'Deleting old requests without screenshots...'
 DELETE FROM requests_request
 WHERE screenshot_id IS NULL
 AND EXISTS (SELECT 1 FROM requests_requestgroup
     WHERE id = requests_request.request_group_id
     AND submitted < NOW() - '7d'::interval);
 
--- Delete old request groups without requests.
+\echo 'Deleting old request groups without requests...'
 DELETE FROM requests_requestgroup
 WHERE submitted < NOW() - '7d'::interval
 AND NOT EXISTS (SELECT 1 FROM requests_request
     WHERE request_group_id = requests_requestgroup.id);
 
--- Delete old websites without request groups.
+\echo 'Deleting old websites without request groups...'
 DELETE FROM websites_website
 WHERE submitted < NOW() - '7d'::interval
 AND NOT EXISTS (SELECT 1 FROM requests_requestgroup
@@ -23,10 +23,18 @@ AND NOT EXISTS (SELECT 1 FROM requests_requestgroup
 AND NOT EXISTS (SELECT 1 FROM screenshots_screenshot
     WHERE website_id = screenshots_screenshot.id);
 
--- Delete old domains without websites.
+\echo 'Deleting old domains without websites...'
 DELETE FROM websites_domain
 WHERE submitted < NOW() - '7d'::interval
 AND NOT EXISTS (SELECT 1 FROM websites_website
     WHERE domain_id = websites_domain.id)
 AND NOT EXISTS (SELECT 1 FROM priority_domainpriority
     WHERE domain_id = websites_domain.id);
+
+\echo 'Deleting old browsers without requests or screenshots...'
+DELETE FROM browsers_browser
+WHERE last_upload < NOW() - '30d'::interval
+AND NOT EXISTS (SELECT 1 FROM screenshots_screenshot
+    WHERE browser_id = browsers_browser.id)
+AND NOT EXISTS (SELECT 1 FROM requests_request
+    WHERE browser_id = browsers_browser.id);
