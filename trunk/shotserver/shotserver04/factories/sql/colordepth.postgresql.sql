fdc3679a1b49cdfded4984b72f6bec28677e228d
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-05-21 21:28:52 +0000

    Removed custom SQL for DELETE CASCADE on foreign keys because the key name is not predictable.

--- trunk/shotserver/shotserver04/factories/sql/colordepth.postgresql.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE factories_colordepth
-DROP CONSTRAINT factories_colordepth_factory_id_fkey;
-ALTER TABLE factories_colordepth
-ADD CONSTRAINT factories_colordepth_factory_id_fkey
-FOREIGN KEY (factory_id) REFERENCES factories_factory(id)
-ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED;
