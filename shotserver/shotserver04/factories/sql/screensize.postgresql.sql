ea6ecb3ff498839eab8e6dbb8030f4144d1de18d
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-05-21 21:28:52 +0000

    Removed custom SQL for DELETE CASCADE on foreign keys because the key name is not predictable.

--- shotserver/shotserver04/factories/sql/screensize.postgresql.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE factories_screensize
-DROP CONSTRAINT factories_screensize_factory_id_fkey;
-ALTER TABLE factories_screensize
-ADD CONSTRAINT factories_screensize_factory_id_fkey
-FOREIGN KEY (factory_id) REFERENCES factories_factory(id)
-ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED;
