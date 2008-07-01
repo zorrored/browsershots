2cf3845112695cbfcd7011c80f3b4a447edd2ae8
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-07-01 02:00:58 +0000

    Added methods for submitting priority requests through XML-RPC.

--- trunk/shotserver/shotserver04/nonces/xmlrpc.py
+++ trunk/shotserver/shotserver04/nonces/xmlrpc.py
@@ -23,6 +23,7 @@ __date__ = "$Date$"
 __author__ = "$Author$"
 
 from xmlrpclib import Fault
+from django.contrib.auth.models import User
 from shotserver04.xmlrpc import signature, factory_xmlrpc
 from shotserver04.nonces import crypto
 from shotserver04.nonces.models import Nonce
@@ -103,6 +104,111 @@ def verify(http_request, factory, encrypted_password):
         algo, salt, hashed = 'md5', '', password
     # Get matching nonces
     nonces = Nonce.objects.filter(factory=factory).extra(
+        where=["MD5(%s || hashkey) = %s"],
+        params=[hashed, encrypted_password])
+    if len(nonces) == 0:
+        raise Fault(401, "Password mismatch.")
+    if len(nonces) > 1:
+        raise Fault(401, "Authentication failed (hash collision).")
+    nonce = nonces[0]
+    # Check IP address
+    if nonce.ip != ip:
+        raise Fault(401, "Authentication failed (different IP address).")
+    # Check nonce freshness
+    if datetime.now() - nonce.created > timedelta(minutes=10):
+        nonce.delete()
+        raise Fault(408, "Authentication failed (nonce expired).")
+    # Success!
+    nonce.delete()
+    return True
+
+
+@signature(dict, str)
+def challengeUser(http_request, username):
+    """
+    Generate a nonce for authentication.
+
+    Arguments
+    ~~~~~~~~~
+    * username string (your user account on the server)
+
+    Return value
+    ~~~~~~~~~~~~
+    * challenge dict
+
+    The return value is a dict with the following keys:
+
+    * algorithm string (sha1 or md5)
+    * salt string (few random characters)
+    * nonce string (random lowercase hexadecimal, length 32)
+
+    See nonces.verifyUser for how to encrypt your password with the
+    nonce.
+    """
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        raise Fault(404, "User not found.")
+    hashkey = crypto.random_md5()
+    ip = http_request.META['REMOTE_ADDR']
+    Nonce.objects.create(email=username, hashkey=hashkey, ip=ip)
+    password = user.password
+    if password.count('$'):
+        algorithm, salt, hashed = password.split('$')
+    else:
+        algorithm, salt, hashed = 'md5', '', password
+    return {
+        'algorithm': algorithm,
+        'salt': salt,
+        'nonce': hashkey,
+        }
+
+
+@signature(bool, str, str)
+def verifyUser(http_request, username, encrypted_password):
+    """
+    Test authentication with an encrypted password.
+
+    Arguments
+    ~~~~~~~~~
+    * username string (your user account on the server)
+    * encrypted_password string (lowercase hexadecimal, length 32)
+
+    Return value
+    ~~~~~~~~~~~~
+    * success boolean (or XML-RPC fault with error message)
+
+    Password encryption
+    ~~~~~~~~~~~~~~~~~~~
+    To encrypt the password, you must first generate a nonce and get
+    the encryption algorithm and salt (see nonces.challengeUser). Then
+    you can compute the encrypted password like this::
+
+        encrypted_password = md5(sha1(salt + password) + nonce)
+
+    If requested by the challenge, you must use md5 rather than sha1
+    for the inner hash. The result of each hash function call must be
+    formatted as lowercase hexadecimal. The calls to
+    nonces.challengeUser and nonces.verifyUser must be made from the
+    same IP address.
+    """
+    if isinstance(username, User):
+        user = username
+        username = user.username
+    else:
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise Fault(404, "User not found.")
+    ip = http_request.META['REMOTE_ADDR']
+    # Get password hash from database
+    password = user.password
+    if password.count('$'):
+        algo, salt, hashed = password.split('$')
+    else:
+        algo, salt, hashed = 'md5', '', password
+    # Get matching nonces
+    nonces = Nonce.objects.filter(email=username).extra(
         where=["MD5(%s || hashkey) = %s"],
         params=[hashed, encrypted_password])
     if len(nonces) == 0:
--- trunk/shotserver/shotserver04/requests/xmlrpc.py
+++ trunk/shotserver/shotserver04/requests/xmlrpc.py
@@ -27,12 +27,17 @@ import random
 from xmlrpclib import Fault
 from django.db import models
 from django.conf import settings
-from shotserver04.common import serializable
+from django.contrib.auth.models import User
+from shotserver04.common import serializable, int_or_none, lock_timeout
+from shotserver04.common.object_cache import preload_foreign_keys
 from shotserver04.xmlrpc import signature, factory_xmlrpc
 from shotserver04.nonces import xmlrpc as nonces
+from shotserver04.websites import normalize_url, extract_domain
+from shotserver04.websites.models import Domain, Website
 from shotserver04.factories.models import Factory, ScreenSize, ColorDepth
-from shotserver04.browsers.models import Browser
-from shotserver04.requests.models import Request
+from shotserver04.platforms.models import Platform
+from shotserver04.browsers.models import BrowserGroup, Browser
+from shotserver04.requests.models import RequestGroup, Request
 from datetime import datetime, timedelta
 # import time # For test_overload.py
 
@@ -230,3 +235,141 @@ def select_color_depth(factory, request):
         return ColorDepth(factory=factory, bits_per_pixel=24)
     # Return greatest matching color depth
     return color_depths[0]
+
+
+def find_by_name(candidates, name):
+    name = name.lower()
+    for candidate in candidates:
+        if candidate.name.lower() == name:
+            return candidate
+
+
+@signature(int, str, str, str, list)
+def submit(http_request, username, encrypted_password, url, browsers):
+    """
+    Submit a new group of screenshot requests.
+
+    Arguments
+    ~~~~~~~~~
+    * username string (your user account on the server)
+    * encrypted_password string (lowercase hexadecimal, length 32)
+    * url string (request screenshots of this website)
+    * browsers list (platform, name and version for each browser)
+
+    Return value
+    ~~~~~~~~~~~~
+    * id int (request group id from requests.submit)
+
+    You can use the returned request group id to check the progress of
+    the screenshot requests with requests.status.
+    """
+    # Get user from database
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        raise Fault(404, "User not found.")
+    # Verify authentication
+    nonces.verifyUser(http_request, user, encrypted_password)
+    # Check priority
+    priority = 0
+    if 'shotserver04.priority' in settings.INSTALLED_APPS:
+        from shotserver04.priority import user_priority
+        priority = user_priority(user)
+        if priority < 1:
+            raise Fault(402, "Priority processing is required.")
+    # Create domain and website if needed
+    url = normalize_url(url)
+    domain_name = extract_domain(url, remove_www=True)
+    domain = Domain.objects.get_or_create(name=domain_name)[0]
+    website = Website.objects.get_or_create(domain=domain, url=url)[0]
+    # Create request group
+    expire = datetime.now() + timedelta(minutes=30)
+    ip = http_request.META['REMOTE_ADDR']
+    request_group = RequestGroup.objects.create(
+        website=website, user=user, ip=ip,
+        expire=expire, priority=priority)
+    # Create browser requests
+    platforms = Platform.objects.all()
+    browser_groups = BrowserGroup.objects.all()
+    for name in browsers:
+        parts = name.split('_')
+        if len(parts) != 4:
+            continue
+        platform_name, browser_name, major, minor = parts
+        platform_name = platform_name.replace('-', ' ')
+        platform = find_by_name(platforms, platform_name)
+        if platform is None:
+            continue
+        browser_group = find_by_name(browser_groups, browser_name)
+        if browser_group is None:
+            continue
+        Request.objects.get_or_create(
+            request_group=request_group,
+            platform=platform,
+            browser_group=browser_group,
+            major=int_or_none(major),
+            minor=int_or_none(minor),
+            priority=priority,
+            )
+    return request_group.id
+
+
+@signature(list, int)
+def status(http_request, request_group_id):
+    """
+    List the status of all screenshot requests in a group.
+
+    Arguments
+    ~~~~~~~~~
+    * id int (request group id from requests.submit)
+
+    Return value
+    ~~~~~~~~~~~~
+    * status list (request status for each requested browser)
+
+    The list will contain a dictionary for each browser, with the
+    following entries:
+
+    * browser string (browser name)
+    * status string (pending / starting / loading / uploaded / failed)
+    * seconds int (estimated or actual time between request and upload)
+    """
+    try:
+        request_group = RequestGroup.objects.get(id=request_group_id)
+    except RequestGroup.DoesNotExist:
+        raise Fault(404, "Request group not found.")
+    results = []
+    matching_browsers = request_group.matching_browsers()
+    requests = request_group.request_set.all()
+    preload_foreign_keys(requests, browser_group=True, platform=True)
+    this_lock_timeout = lock_timeout()
+    for request in requests:
+        platform_name = request.platform.name.lower()
+        browser_name = request.browser_group.name.lower()
+        major = str(request.major)
+        minor = str(request.minor)
+        name = '_'.join((platform_name, browser_name, major, minor))
+        name = name.replace(' ', '-')
+        if request.screenshot_id is not None:
+            status = 'uploaded'
+            seconds = (request.screenshot.uploaded -
+                       request_group.submitted).seconds
+        elif request.locked and request.locked < this_lock_timeout:
+            status = 'failed'
+            seconds = 0
+        elif request.redirected:
+            status = 'loading'
+            seconds = (request.redirected -
+                       request_group.submitted).seconds + 45
+        elif request.locked:
+            status = 'starting'
+            seconds = (request.locked -
+                       request_group.submitted).seconds + 60
+        else:
+            status = 'pending'
+            seconds = request.queue_estimate(matching_browsers) or 0
+        results.append({'browser': name,
+                        'status': status,
+                        'seconds': seconds,
+                        })
+    return results
