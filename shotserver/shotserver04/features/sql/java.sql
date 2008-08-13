474256635cd0bf681e34e37e209a720b77792a81
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-08-13 21:59:46 +0000

    Removed custom SQL that was replaced by fixtures.

--- shotserver/shotserver04/features/sql/java.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO "features_java" ("id","version") VALUES (1,'disabled');
-INSERT INTO "features_java" ("id","version") VALUES (2,'enabled');
-INSERT INTO "features_java" ("id","version") VALUES (3,'1.3');
-INSERT INTO "features_java" ("id","version") VALUES (4,'1.4');
-INSERT INTO "features_java" ("id","version") VALUES (5,'1.5');
-INSERT INTO "features_java" ("id","version") VALUES (6,'1.6');
-SELECT setval('features_java_id_seq', (SELECT max("id") FROM "features_java"));
