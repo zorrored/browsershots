549de55f69bfec344be1d1c465ef633984f0fc36
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-08-13 22:03:45 +0000

    Added header.

--- shotserver/shotserver04/common/results.py
+++ shotserver/shotserver04/common/results.py
@@ -1,3 +1,27 @@
+# browsershots.org - Test your web design in different browsers
+# Copyright (C) 2007 Johann C. Rocholl <johann@browsershots.org>
+#
+# Browsershots is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Browsershots is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Utilities for the results of HTTP POST requests.
+"""
+
+__revision__ = "$Rev: 1464 $"
+__date__ = "$Date: 2007-06-08 13:12:43 -0700 (Fri, 08 Jun 2007) $"
+__author__ = "$Author: johann $"
+
 from django.utils.translation import ugettext as _
 from django.http import HttpResponseRedirect
 from django.utils.text import capfirst
