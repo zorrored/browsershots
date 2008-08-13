5ea71023a33f7ba862f49639c8b5ea67a945c77f
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-08-13 21:59:46 +0000

    Removed custom SQL that was replaced by fixtures.

--- trunk/plugins/revenue/sql/nonprofit.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO "revenue_nonprofit" ("id","name","url") VALUES (1,'Python Software Foundation','http://www.python.org/psf/');
-INSERT INTO "revenue_nonprofit" ("id","name","url") VALUES (2,'Free Software Foundation','http://www.fsf.org/');
-INSERT INTO "revenue_nonprofit" ("id","name","url") VALUES (3,'Free Software Foundation Europe','http://www.fsfeurope.org/');
-SELECT setval('revenue_nonprofit_id_seq', (SELECT max("id") FROM "revenue_nonprofit"));
--- trunk/shotserver/shotserver04/platforms/sql/platform.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO "platforms_platform" ("id","name","position") VALUES (1,'Linux',1);
-INSERT INTO "platforms_platform" ("id","name","position") VALUES (2,'Mac OS',3);
-INSERT INTO "platforms_platform" ("id","name","position") VALUES (3,'Windows',2);
-INSERT INTO "platforms_platform" ("id","name","position") VALUES (4,'BSD',4);
-SELECT setval('platforms_platform_id_seq', (SELECT max("id") FROM "platforms_platform"));
