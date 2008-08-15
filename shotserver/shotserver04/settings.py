16604b30a70f9dd73ba8530bb7d3bce6b31c8271
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-08-15 15:55:02 +0000

    Added trailing slash for CACHE_BACKEND memcached, for consistency with Django docs.

--- shotserver/shotserver04/settings.py
+++ shotserver/shotserver04/settings.py
@@ -44,8 +44,8 @@ DATABASE_HOST = ''             # Set to empty string for localhost.
 DATABASE_PORT = ''             # Set to empty string for default.
 
 CACHE_BACKEND = 'locmem:///'
+# CACHE_BACKEND = "memcached://127.0.0.1:11211/"
 # CACHE_BACKEND = 'file:///var/cache/shotserver04'
-# CACHE_BACKEND = "memcached://127.0.0.1:11211"
 
 # Local time zone for this installation. All choices can be found here:
 # http://www.postgresql.org/docs/8.1/static/datetime-keywords.html
