4b2c3316bd3597ff6b4dd2c3017b5bc1f61c5d16
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-08-24 21:22:18 +0000

    More test cases.

--- shotserver/shotserver05/factories/tests.py
+++ shotserver/shotserver05/factories/tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
+from django.test.client import Client
 from django.contrib.auth.models import User
 from shotserver05.platforms.models import OperatingSystem
 from shotserver05.factories.models import Factory
+from shotserver05.factories import xmlrpc as factories
+from shotserver05.xmlrpc.tests import \
+    TestServerProxy, authenticate, TESTCLIENT_PASSWORD, FACTORY1_SECRET
 
 
 class FactoryTestCase(TestCase):
@@ -18,3 +22,31 @@ class FactoryTestCase(TestCase):
     def testRelated(self):
         self.assertEqual(self.factory1.screensize_set.count(), 1)
         self.assertEqual(self.factory1.colordepth_set.count(), 1)
+
+    def testCreate(self):
+        self.factory2 = Factory.objects.create(
+            name='factory2',
+            user=User.objects.get(username='testclient'),
+            operating_system=OperatingSystem.objects.get(slug='leopard'))
+
+
+class XMLRPCTestCase(TestCase):
+    fixtures = ['authtestdata', 'test_factories']
+
+    def setUp(self):
+        self.server = TestServerProxy(self.client)
+
+    def testCreateFactory(self):
+        args = ['testclient', 'smug', 'leopard', 'MacBook']
+        authenticate('factories.createFactory', args, TESTCLIENT_PASSWORD)
+        response = self.server.factories.createFactory(*args)
+        self.assertEqual(response, 'OK')
+
+    def testDetails(self):
+        details = self.server.factories.details('factory1')
+        self.assertEqual(details['name'], 'factory1')
+        self.assertEqual(details['operating_system'], 'leopard')
+        self.assertEqual(details['hardware'], 'MacBook')
+        self.assertEqual(details['last_poll'], '')
+        self.assertEqual(details['last_upload'], '')
+        self.assertEqual(details['last_error'], '')
--- shotserver/shotserver05/factories/xmlrpc.py
+++ shotserver/shotserver05/factories/xmlrpc.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from shotserver05.xmlrpc.utils import user_auth, factory_auth
 from shotserver05.factories.models import Factory
+from shotserver05.platforms.models import OperatingSystem
 from shotserver05.factories.utils import last_poll_timeout
 
 
@@ -32,40 +33,37 @@ def details(request, factory_name):
 
     The return dict will contain the following entries:
 
-    * factory_name string (lowercase)
-    * operating_system_slug string (see platforms.listOperatingSystems)
+    * name string (lowercase)
+    * operating_system string (see platforms.listOperatingSystems)
     * hardware string (e.g. MacBook, Intel Core Duo, 2 GHz, 2 GB)
     * last_poll string (UTC, ISO 8601: YYYY-MM-DDThh:mm:ssZ)
     * last_upload string (UTC, ISO 8601: YYYY-MM-DDThh:mm:ssZ)
-    * uploads_per_hour int (in the last 60 minutes)
-    * uploads_per_day int (in the last 24 hours)
-    * errors_per_hour int (in the last 60 minutes)
-    * errors_per_day int (in the last 24 hours)
-    * problems_per_day int (in the last 24 hours)
+    * last_error string (UTC, ISO 8601: YYYY-MM-DDThh:mm:ssZ)
     """
     factory = get_object_or_404(Factory, name=factory_name)
-    result = {}
-    for key, value in factory.__dict__.iteritems():
-        if 'secret' in key:
-            continue
-        if value is None or isinstance(value, datetime):
-            value = value.astimezone(None).isoformat()
-        result[key] = value
-    return result
+    return {
+        'name': factory.name,
+        'operating_system': factory.operating_system.slug,
+        'hardware': factory.hardware,
+        'last_poll': factory.last_poll or '',
+        'last_upload': factory.last_upload or '',
+        'last_error': factory.last_error or '',
+        }
 
 
 ##################### Methods for authenticated user #########################
 
 
 @user_auth
-def create(request, user, factory_name, operating_system_slug, hardware):
+def createFactory(request, user, factory_name, operating_system, hardware):
     """
     Create a new screenshot factory.
 
     Arguments:
     ~~~~~~~~~~
+    * username string (regular Django user account)
     * factory_name string (lowercase, use hostname if possible)
-    * operating_system_slug string (see platforms.listOperatingSystems)
+    * operating_system string (see platforms.listOperatingSystems)
     * hardware string (e.g. MacBook, Intel Core Duo, 2 GHz, 2 GB)
 
     Return value:
@@ -73,8 +71,8 @@ def create(request, user, factory_name, operating_system_slug, hardware):
     * status string (OK)
     """
     operating_system = get_object_or_404(
-        OperatingSystem, slug=operating_system_slug)
-    Factory.objects.create(name=name, user=user,
+        OperatingSystem, slug=operating_system)
+    Factory.objects.create(name=factory_name, user=user,
         operating_system=operating_system, hardware=hardware)
     return 'OK'
 
@@ -96,6 +94,7 @@ def testAuth(request, factory, dummy_number, dummy_text):
 
     Arguments:
     ~~~~~~~~~~
+    * factory_name string (lowercase)
     * dummy_number int (e.g. 123)
     * dummy_text string (e.g. hello)
 
@@ -113,6 +112,7 @@ def update(request, factory, operating_system_slug, hardware):
 
     Arguments:
     ~~~~~~~~~~
+    * factory_name string (lowercase)
     * operating_system_slug string (see platforms.listOperatingSystems)
     * hardware string (e.g. MacBook, Intel Core Duo, 2 GHz, 2 GB)
 
