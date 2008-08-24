8f83d9d074328168843327c0d5c702dcdcc61122
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-08-24 21:18:45 +0000

    Removed uploads_per_day and uploads_per_hour from Factory model, added last_error field and new FactoryStatistics model.

--- shotserver/shotserver05/factories/models.py
+++ shotserver/shotserver05/factories/models.py
@@ -10,11 +10,10 @@ class Factory(models.Model):
     secret_key = models.CharField(max_length=512, default=random_secret_key)
     operating_system = models.ForeignKey(OperatingSystem)
     hardware = models.CharField(max_length=200)
-    uploads_per_day = models.IntegerField(editable=False, null=True)
-    uploads_per_hour = models.IntegerField(editable=False, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     last_upload = models.DateTimeField(editable=False, null=True)
     last_poll = models.DateTimeField(editable=False, null=True)
+    last_error = models.DateTimeField(editable=False, null=True)
 
     class Meta:
         ordering = ('name', )
@@ -48,3 +47,14 @@ class ColorDepth(models.Model):
 
     def __unicode__(self):
         return str(self.bits_per_pixel)
+
+
+class FactoryStatistics(models.Model):
+    factory = models.ForeignKey(Factory)
+    date = models.DateField()
+    screenshot_count = models.IntegerField()
+    error_count = models.IntegerField()
+    problem_count = models.IntegerField()
+
+    class Meta:
+        ordering = ('date', 'screenshot_count')
