f9b3c24d8118bd9811e3b9b640413a98591294e1
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-08-25 16:27:11 +0000

    Allow anonymous job groups, added hashkey.

--- shotserver/shotserver05/jobs/models.py
+++ shotserver/shotserver05/jobs/models.py
@@ -6,8 +6,9 @@ from shotserver05.websites.models import Website
 
 
 class Group(models.Model):
+    hashkey = models.SlugField(max_length=32)
     website = models.ForeignKey(Website)
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, blank=True, null=True)
     priority = models.IntegerField()
     submitted = models.DateTimeField(auto_now_add=True)
 
