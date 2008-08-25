3e5146b2412a8283be3d3932c7b80913296cf9be
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-08-25 16:26:03 +0000

    Added submitted field and __unicode__ method.

--- shotserver/shotserver05/websites/models.py
+++ shotserver/shotserver05/websites/models.py
@@ -3,8 +3,16 @@ from django.db import models
 
 class Domain(models.Model):
     domain = models.CharField(max_length=200)
+    submitted = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.domain
 
 
 class Website(models.Model):
     url = models.CharField(max_length=400)
     domain = models.ForeignKey(Domain)
+    submitted = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.url
