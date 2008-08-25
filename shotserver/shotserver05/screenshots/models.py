45ca95e50a34d18e49b78cf679a22dbaedab71b8
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-08-25 16:25:26 +0000

    Moved hashkey from Screenshot to Attempt model.

--- shotserver/shotserver05/screenshots/models.py
+++ shotserver/shotserver05/screenshots/models.py
@@ -6,17 +6,23 @@ from shotserver05.factories.models import Factory
 
 class Attempt(models.Model):
     job = models.ForeignKey(Job)
+    hashkey = models.SlugField(max_length=32)
     factory = models.ForeignKey(Factory)
     started = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.hashkey
 
 
 class Screenshot(models.Model):
     attempt = models.ForeignKey(Attempt)
-    hashkey = models.CharField(max_length=32, blank=True, null=True)
     width = models.IntegerField()
     height = models.IntegerField()
     bytes = models.IntegerField()
     uploaded = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return '%sx%s' % (self.width, self.height)
 
 
 class Error(models.Model):
@@ -25,6 +31,9 @@ class Error(models.Model):
     message = models.CharField(max_length=400)
     occurred = models.DateTimeField(auto_now_add=True)
 
+    def __unicode__(self):
+        return self.message
+
 
 class Problem(models.Model):
     screenshot = models.ForeignKey(Screenshot)
@@ -32,3 +41,6 @@ class Problem(models.Model):
     message = models.CharField(max_length=400)
     reporter = models.ForeignKey(User)
     reported = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.message
