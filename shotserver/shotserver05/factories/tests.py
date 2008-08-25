10d443375bf94f6fa77970de13374ba011564c0b
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-08-25 03:46:57 +0000

    Moved tests for testAuth to factories and users apps.

--- shotserver/shotserver05/factories/tests.py
+++ shotserver/shotserver05/factories/tests.py
@@ -4,9 +4,20 @@ from django.contrib.auth.models import User
 from shotserver05.platforms.models import OperatingSystem
 from shotserver05.factories.models import Factory
 from shotserver05.factories import xmlrpc as factories
-from shotserver05.xmlrpc.tests import \
-    TestServerProxy, authenticate, TESTCLIENT_PASSWORD, FACTORY1_SECRET
+from shotserver05.xmlrpc.tests import TestServerProxy, authenticate
+from shotserver05.users.tests import TESTCLIENT_PASSWORD
 from shotserver05.system.utils import signature
+
+FACTORY1_SECRET = ''.join("""
+xEcYUVx+3H4tFABMleVTm6DFd9NW1Z7cDJWQNnMWEP19jPrj0EMi8ux8Kp1uutiv
+4Xf/UOLeOvpW3A5vX/0+aZT4B+ktsT+6j/50MjceG5bQY4pmVf1cg4JKqgl4FdOY
+wd4d6DReY8uCXa8WUexiIuQvGdGHqk2wsBypVlnfZTZMHzHG4ivdufRXzgTE6+Ar
+VbiQLrIXsvElzbkrETcqxefvtNX23cjTXqWZo7XoL5tk5u6d2pjEn/DM6JGWXtWx
+ZaraPQpTaEZx5T8NGva0DDcFJewJH4jMTer9UqQOf57Ld73+XAz7U3AEm7lsezNQ
+6hmq3MxgKJVm9zCdIIxQh0EmOLQkrmajw2pLM8JMoHxBoxvaQKe6TkxRBnkSIQVF
+CMT326GQKUFzowhEZipydkp4myc7XctfyZ6gBIXUXHaRBd/4hYyF0i3XJX2cDMt9
+5zbQncx+k44N4c+N/VKg2c8cOezIrtXcotKQ7Bxet2/u1ZGIEc2yR45f3iwwRqQ5
+""".split())
 
 
 class FactoryTestCase(TestCase):
@@ -36,6 +47,14 @@ class XMLRPCTestCase(TestCase):
 
     def setUp(self):
         self.server = TestServerProxy(self.client)
+
+    def testAuth(self):
+        self.assertEqual(
+            signature('factories.testAuth'),
+            ['string', 'string', 'int', 'string', 'string', 'string'])
+        args = ['factory1', 123, 'hello']
+        authenticate('factories.testAuth', args, FACTORY1_SECRET)
+        self.assertEquals(self.server.factories.testAuth(*args), 'OK')
 
     def testCreateFactory(self):
         self.assertEqual(signature('factories.createFactory'), 7 * ['string'])
