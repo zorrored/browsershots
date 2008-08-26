0b88cb9440af485780c838cbabb2927f4be9d6c2
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-08-26 15:49:18 +0000

    Renamed model jobs.Group to JobGroup.

--- branches/0.5/shotserver/shotserver05/jobs/models.py
+++ branches/0.5/shotserver/shotserver05/jobs/models.py
@@ -5,17 +5,24 @@ from shotserver05.platforms.models import Platform
 from shotserver05.browsers.models import BrowserName
 
 
-class Group(models.Model):
-    hashkey = models.SlugField(max_length=32)
+class JobGroup(models.Model):
+    hashkey = models.SlugField(max_length=32, unique=True)
     website = models.ForeignKey(Website)
     user = models.ForeignKey(User, blank=True, null=True)
     priority = models.PositiveIntegerField(default=0)
     submitted = models.DateTimeField(auto_now_add=True)
 
+    def __unicode__(self):
+        return self.website.url
+
 
 class Job(models.Model):
-    group = models.ForeignKey(Group)
+    group = models.ForeignKey(JobGroup)
     platform = models.ForeignKey(Platform)
     browser_name = models.ForeignKey(BrowserName)
     major = models.PositiveIntegerField()
     minor = models.PositiveIntegerField()
+
+    def __unicode__(self):
+        return '%s %s %s.%s' % (self.platform.name,
+            self.browser_name.name, self.major, self.minor)
