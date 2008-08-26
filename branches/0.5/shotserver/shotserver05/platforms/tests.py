d8748981e3f640f6781c26c63703bb08092a82a6
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-08-26 16:22:34 +0000

    Added tests for XML-RPC methods.

--- branches/0.5/shotserver/shotserver05/platforms/tests.py
+++ branches/0.5/shotserver/shotserver05/platforms/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.db import transaction
+from shotserver05.system.utils import signature
+from shotserver05.xmlrpc.tests import TestServerProxy
 from shotserver05.platforms.models import Platform, OperatingSystem
 
 
@@ -11,3 +13,27 @@ class PlatformTestCase(TestCase):
     def testSlug(self):
         for platform in Platform.objects.all():
             self.assertEqual(platform.name.lower(), platform.slug)
+
+
+class XMLRPCTestCase(TestCase):
+
+    def setUp(self):
+        transaction.rollback()
+        self.server = TestServerProxy(self.client)
+
+    def testListOperatingSystems(self):
+        self.assertEqual(signature('platforms.listOperatingSystems'),
+                         ['list'])
+        os_list = self.server.platforms.listOperatingSystems()
+        self.assert_('leopard' in os_list)
+        self.assert_('xp' in os_list)
+        self.assert_('hardy' in os_list)
+
+    def testOperatingSystemDetails(self):
+        self.assertEqual(signature('platforms.operatingSystemDetails'),
+                         ['dict', 'string'])
+        details = self.server.platforms.operatingSystemDetails('leopard')
+        self.assertEqual(details['platform'], 'Mac')
+        self.assertEqual(details['name'], 'Mac OS X')
+        self.assertEqual(details['version'], '10.5')
+        self.assertEqual(details['codename'], 'Leopard')
