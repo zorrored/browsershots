df0999f62049947eab9bb462121ce8c95183dc74
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-08-26 00:46:05 +0000

    Adjusted IntegerFields to PositiveIntegerField, added default value.

--- branches/0.5/shotserver/shotserver05/jobs/models.py
+++ branches/0.5/shotserver/shotserver05/jobs/models.py
@@ -9,7 +9,7 @@ class Group(models.Model):
     hashkey = models.SlugField(max_length=32)
     website = models.ForeignKey(Website)
     user = models.ForeignKey(User, blank=True, null=True)
-    priority = models.IntegerField()
+    priority = models.PositiveIntegerField(default=0)
     submitted = models.DateTimeField(auto_now_add=True)
 
 
@@ -17,5 +17,5 @@ class Job(models.Model):
     group = models.ForeignKey(Group)
     platform = models.ForeignKey(Platform)
     browser_name = models.ForeignKey(BrowserName)
-    major = models.IntegerField()
-    minor = models.IntegerField()
+    major = models.PositiveIntegerField()
+    minor = models.PositiveIntegerField()
