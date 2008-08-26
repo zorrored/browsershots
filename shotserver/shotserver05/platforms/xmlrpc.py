36a95f14a74d3e6ca350bd9e6eb3ff8955cd6eee
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-08-26 16:22:17 +0000

    Added XML-RPC method platforms.operatingSystemDetails.

--- shotserver/shotserver05/platforms/xmlrpc.py
+++ shotserver/shotserver05/platforms/xmlrpc.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from shotserver05.platforms.models import OperatingSystem
 
 
@@ -10,3 +11,31 @@ def listOperatingSystems(request):
     * slugs list (a slug for each operating system)
     """
     return [os.slug for os in OperatingSystem.objects.all()]
+
+
+def operatingSystemDetails(request, operating_system):
+    """
+    Get details for the specified operating system.
+
+    Arguments:
+    ~~~~~~~~~~
+    * operating_system string
+
+    Return value:
+    ~~~~~~~~~~~~~
+    * details dict
+
+    The result dict will contain the following values:
+
+    * platform string
+    * name string
+    * version string
+    * codename string
+    """
+    os = get_object_or_404(OperatingSystem, slug=operating_system)
+    return {
+        'platform': os.platform.name,
+        'name': os.name,
+        'version': os.version,
+        'codename': os.codename,
+        }
