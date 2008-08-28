14f7fffef9f3dc3d5ff490b170c9ead8ac2c04e5
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-08-28 18:41:25 +0000

    Renamed PositiveIntegerField to UnsignedIntegerField.

--- shotserver/shotserver05/jobs/models.py
+++ shotserver/shotserver05/jobs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import PositiveIntegerField as UnsignedIntegerField
 from shotserver05.websites.models import Website
 from django.contrib.auth.models import User
 from shotserver05.platforms.models import Platform
@@ -9,7 +10,7 @@ class JobGroup(models.Model):
     hashkey = models.SlugField(max_length=32, unique=True)
     website = models.ForeignKey(Website)
     user = models.ForeignKey(User, blank=True, null=True)
-    priority = models.PositiveIntegerField(default=0)
+    priority = UnsignedIntegerField(default=0)
     submitted = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
@@ -20,8 +21,8 @@ class Job(models.Model):
     group = models.ForeignKey(JobGroup)
     platform = models.ForeignKey(Platform)
     browser_name = models.ForeignKey(BrowserName)
-    major = models.PositiveIntegerField()
-    minor = models.PositiveIntegerField()
+    major = UnsignedIntegerField()
+    minor = UnsignedIntegerField()
 
     def __unicode__(self):
         return '%s %s %s.%s' % (self.platform.name,
