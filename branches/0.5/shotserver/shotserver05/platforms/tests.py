d1194fc1db3b860aee245a794ef826e7b27b3310
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-09-10 16:20:31 +0000

    Added headers and docstrings.

--- branches/0.5/shotserver/shotserver05/platforms/tests.py
+++ branches/0.5/shotserver/shotserver05/platforms/tests.py
@@ -1,3 +1,27 @@
+# browsershots.org - Test your web design in different browsers
+# Copyright (C) 2008 Johann C. Rocholl <johann@browsershots.org>
+#
+# Browsershots is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Browsershots is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Browsershots. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Tests for the platforms app.
+"""
+
+__revision__ = "$Rev$"
+__date__ = "$Date$"
+__author__ = "$Author$"
+
 from django.test import TestCase
 from django.db import transaction
 from shotserver05.system.utils import signature
