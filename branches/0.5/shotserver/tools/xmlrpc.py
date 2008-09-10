ee0a56e0d5b476fefb5110c83608c4c3abe202f5
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-09-10 16:45:39 +0000

    Added headers and docstrings.

--- branches/0.5/shotserver/tools/xmlrpc.py
+++ branches/0.5/shotserver/tools/xmlrpc.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python
+# browsershots.org - Test your web design in different browsers
+# Copyright (C) 2008 Johann C. Rocholl <johann@browsershots.org>
+#
+# Browsershots is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Browsershots is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Browsershots. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Generic command-line client for XML-RPC interfaces.
+"""
+
+__revision__ = "$Rev$"
+__date__ = "$Date$"
+__author__ = "$Author$"
 
 import os
 import xmlrpclib
@@ -9,7 +32,7 @@ DEFAULT_SERVER = 'http://127.0.0.1:8000/xmlrpc/'
 
 
 def _main():
-    usage = "%prog [options] method [args] ..."
+    usage = "%prog [options] <module.method> [args] ..."
     parser = OptionParser(usage)
     parser.add_option('-v', '--verbose', default=0, action='count',
                       help="print status messages, or debug with -vv")
@@ -17,7 +40,7 @@ def _main():
                       help="server url (or XMLRPC_SERVER from environment)")
     options, args = parser.parse_args()
     if len(args) == 0:
-        parser.error('method not specified')
+        parser.error('method not specified, try system.listMethods')
     method = args.pop(0)
     if not options.server:
         if 'XMLRPC_SERVER' in os.environ:
