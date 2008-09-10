19d8859c8b769412d83dd89b8041f5d62ca3a643
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-09-10 16:22:30 +0000

    Added headers and docstrings.

--- shotserver/shotserver05/manage.py
+++ shotserver/shotserver05/manage.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python
+# browsershots.org - Test your web design in different browsers
+# Copyright (C) 2008 Johann C. Rocholl <johann@browsershots.org>
+#
+# Browsershots is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Browsershots is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Browsershots. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Django command-line interface for shotserver05.
+"""
+
+__revision__ = "$Rev$"
+__date__ = "$Date$"
+__author__ = "$Author$"
+
 from django.core.management import execute_manager
 try:
     import settings # Assumed to be in the same directory.
