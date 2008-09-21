3fbb1a6edca359663732f84c9fb3a7cb81b6fa37
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-09-21 23:51:26 +0000

    Use RegexField for factory name.

--- shotserver/shotserver05/factories/forms.py
+++ shotserver/shotserver05/factories/forms.py
@@ -23,13 +23,11 @@ __date__ = "$Date$"
 __author__ = "$Author$"
 
 from django import forms
+from django.utils.translation import ugettext as _
 from shotserver05.factories.models import Factory
 
-LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
-DIGITS = '0123456789'
-FACTORY_NAME_CHARS = LOWERCASE_LETTERS + DIGITS + '_-'
 RESERVED_FACTORY_NAMES = """
-create
+localhost server factory shotfactory create
 """.split()
 
 
@@ -37,32 +35,34 @@ class CreateFactoryForm(forms.ModelForm):
     """
     Create a new screenshot factory.
     """
-    hardware = forms.CharField(widget=forms.TextInput(attrs={'size': '40'}))
+    name = forms.RegexField(regex=r'[a-z][a-z0-9-_]',
+        max_length=Factory._meta.get_field('name').max_length)
+    hardware = forms.CharField(
+        max_length=Factory._meta.get_field('hardware').max_length,
+        widget=forms.TextInput(attrs={'size': '40'}))
 
     class Meta:
         model = Factory
         exclude = ('user', 'secret_key')
 
     def clean_name(self):
+        """
+        Check the submitted factory name value.
+        """
         name = self.cleaned_data['name'].strip()
         if len(name) == 0:
-            raise forms.ValidationError("This field is required.")
-        if len(name) < 2:
-            raise forms.ValidationError(
-                "Factory name must be at least 2 characters long.")
-        if name[0] not in LOWERCASE_LETTERS:
-            raise forms.ValidationError(
-                "Factory name must start with a lowercase letter.")
-        for letter in name[0]:
-            if letter not in FACTORY_NAME_CHARS:
-                raise forms.ValidationError(
-"Factory name may contain only lowercase letters, digits, hyphen, underscore.")
+            raise forms.ValidationError(_("This field is required."))
+        if name in RESERVED_FACTORY_NAMES:
+            raise forms.ValidationError(_("This name is reserved."))
         return name
 
     def clean_hardware(self):
+        """
+        Check the submitted hardware value.
+        """
         hardware = self.cleaned_data['hardware'].strip()
         if len(hardware) == 0:
-            raise forms.ValidationError("This field is required.")
+            raise forms.ValidationError(_("This field is required."))
         return hardware
 
 
@@ -70,7 +70,9 @@ class FactoryForm(forms.ModelForm):
     """
     Edit factory details.
     """
-    hardware = forms.CharField(widget=forms.TextInput(attrs={'size': '40'}))
+    hardware = forms.CharField(
+        max_length=Factory._meta.get_field('hardware').max_length,
+        widget=forms.TextInput(attrs={'size': '40'}))
 
     clean_hardware = CreateFactoryForm.clean_hardware
 
