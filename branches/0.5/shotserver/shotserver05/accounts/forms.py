ae3479f7d90f85f97716163f6f77c4a6bd5a7277
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2008-09-22 00:35:21 +0000

    Simplified CreateUserForm with RegexField and min_length.

--- branches/0.5/shotserver/shotserver05/accounts/forms.py
+++ branches/0.5/shotserver/shotserver05/accounts/forms.py
@@ -29,59 +29,42 @@ RESERVED_USERNAMES = """
 admin administrator root webmaster www-data
 postmaster test testuser testclient staff create
 """.split()
-LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
-DIGITS = '0123456789'
-USERNAME_CHARS = LOWERCASE_LETTERS + DIGITS + '_-'
-USERNAME_MIN_LENGTH = 2
-PASSWORD_MIN_LENGTH = 6
 
 
 class CreateUserForm(forms.Form):
     first_name = forms.CharField(max_length=40)
     last_name = forms.CharField(max_length=40)
-    username = forms.CharField(max_length=40)
-    password = forms.CharField(max_length=40, widget=forms.PasswordInput)
-    repeat = forms.CharField(max_length=40, widget=forms.PasswordInput)
+    username = forms.RegexField(min_length=2, max_length=40,
+        regex=r'[a-z][a-z0-9-_]+')
+    password = forms.CharField(min_length=6, max_length=40,
+        widget=forms.PasswordInput)
+    repeat = forms.CharField(max_length=40,
+        widget=forms.PasswordInput)
     email = forms.EmailField()
 
     def clean_first_name(self):
         """
         Check that the first name starts with an uppercase letter.
         """
-        first_name = self.cleaned_data['first_name'].strip()
-        if not first_name:
-            return ''
+        first_name = self.cleaned_data['first_name']
         if first_name[0].upper() != first_name[0]:
-            raise forms.ValidationError("Name should start with uppercase.")
+            raise forms.ValidationError("Name must start with uppercase.")
         return first_name
 
     def clean_last_name(self):
         """
         Check that the last name starts with an uppercase letter.
         """
-        last_name = self.cleaned_data['last_name'].strip()
-        if not last_name:
-            return ''
+        last_name = self.cleaned_data['last_name']
         if last_name[0].upper() != last_name[0]:
-            raise forms.ValidationError("Name should start with uppercase.")
+            raise forms.ValidationError("Name must start with uppercase.")
         return last_name
 
     def clean_username(self):
         """
         Check that the username is sensible and available.
         """
-        username = self.cleaned_data['username'].strip()
-        if len(username) < USERNAME_MIN_LENGTH:
-            raise forms.ValidationError(
-                "The username must have at least %d characters." %
-                USERNAME_MIN_LENGTH)
-        if username[0] not in LOWERCASE_LETTERS:
-            raise forms.ValidationError(
-                "Username must start with a lowercase letter.")
-        for index in range(len(username)):
-            if username[index] not in USERNAME_CHARS:
-                raise forms.ValidationError(
-                    "Username may contain only simple letters (a-z0-9_.-).")
+        username = self.cleaned_data['username']
         if username in RESERVED_USERNAMES:
             raise forms.ValidationError("This username is reserved.")
         if User.objects.filter(username=username).count():
@@ -90,13 +73,9 @@ class CreateUserForm(forms.Form):
 
     def clean_password(self):
         """
-        Check that the password is long enough and not too silly.
+        Check that the password is not too silly.
         """
         password = self.cleaned_data['password']
-        if len(password) < PASSWORD_MIN_LENGTH:
-            raise forms.ValidationError(
-                "The password must have at least %d characters." %
-                PASSWORD_MIN_LENGTH)
         if password.isdigit() or password == len(password) * password[0]:
             raise forms.ValidationError("The password is too simple.")
         return password
